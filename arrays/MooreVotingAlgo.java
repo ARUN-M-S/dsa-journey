beb042f9fda548a9d12cce3e6d855c6652fb7911
ARUN-M-S <arunms96official@gmail.com>
2025-04-14 22:22:53 +0530

    voting algo

--- arrays/MooreVotingAlgo.java
+++ arrays/MooreVotingAlgo.java
@@ -1,8 +1,9 @@
+import java.util.HashSet;
 
 public class MooreVotingAlgo {
 
     static int votingAlgo(int[] nums){
-       
+      
         int majority= nums[0];
         int vote=0;
     for(int i =0;i<nums.length;i++){
