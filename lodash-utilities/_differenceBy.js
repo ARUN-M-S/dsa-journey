6072b206548ea08c102854e3e779f9c830078b32
ARUN-M-S <arunms96official@gmail.com>
2025-04-25 19:12:11 +0530

    _differenceBy

--- lodash-utilities/_differenceBy.js
+++ lodash-utilities/_differenceBy.js
@@ -1,8 +1,9 @@
-function _difference(arr, arr2) {
-    if (!Array.isArray(arr) || !Array.isArray(arr2)) return 'Please check inputs'
-    let excludes = new Set(arr2);
-    return arr.filter(val => !excludes.has(val))
+function _differenceBy(arr1, arr2, fn) {
+    if (!Array.isArray(arr1) || !Array.isArray(arr2)) return 'Please check inputs'
+    let exclude = new Set(arr2.map(fn));
+
+    return arr1.filter(val => !exclude.has(fn(val)))
+
 }
 
-
-console.log(_difference([2, 1], [2, 3,4]));
+console.log(_differenceBy([2.1, 1.2], [2.3, 3.4], Math.floor));
