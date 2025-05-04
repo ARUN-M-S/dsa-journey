827c8c93c95878e8dbfa6b5d16748e4d73a88672
ARUN-M-S <arunms96official@gmail.com>
2025-05-04 09:28:05 +0530

    linked list

--- linkedList/singlyLinkedList/singlyLinkedList.java
+++ linkedList/singlyLinkedList/singlyLinkedList.java
@@ -1,5 +1,52 @@
-package dev-journey.dsa-journey.linkedList.slinglyLinkedList;
-
+class Node{
+    int value;
+    Node next;
+    Node(int val){
+        this.value = val;
+        this.next=null;
+    }
+}
 public class singlyLinkedList {
-    
+    Node head,tail;
+    int length;
+    public singlyLinkedList(){
+        this.head = null;
+        this.tail= null;
+        this.length=0;
+    }
+
+    public void push(int val){
+        Node newNode = new Node(val);
+        if(this.head==null){
+            this.head =newNode;
+            this.tail= newNode;
+       
+        }else{
+            this.tail.next=newNode;
+            this.tail=newNode;
+        }
+        this.length++;
+    }
+
+    public Node pop(){
+        if(this.head==null) return  null;
+
+        
+        Node currentNode = this.head;
+        Node newTail = currentNode;
+
+        while(currentNode.next!=null){
+            newTail = currentNode;
+           currentNode=currentNode.next;
+        }
+       this.tail = newTail;
+       this.tail.next=null;
+       this.length--;
+
+    if (this.length == 0) {
+        this.head = null;
+        this.tail = null;
+    }
+       return currentNode;
+    }
 }
