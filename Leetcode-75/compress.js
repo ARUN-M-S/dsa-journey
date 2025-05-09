51f6628d4080647ab9e47670412b4287b6131138
ARUN-M-S <arunms96official@gmail.com>
2025-05-09 08:15:08 +0530

    compress

---
+++ Leetcode-75/compress.js
@@ -0,0 +1,31 @@
+
+ var compress = function(chars) {
+    
+    let current= chars[0];
+    let count =1;
+    let result = []
+
+    for(let i=1;i<chars.length;i++){
+       if(current ==chars[i]){
+        count++
+       }else{
+        result.push(current);
+         if(count>1)result.push(...count.toString());
+        current=chars[i];
+        count=1;
+       }
+      
+    }
+    result.push(current);
+    if (count > 1) result.push(...count.toString());
+   for (let i = 0; i < result.length; i++) {
+        chars[i] = result[i];
+    }
+
+    return result.length;
+};
+
+console.log(compress(["a","a","b","b","c","c","c"]))
+
+console.log(compress(["a","b","b","b","b","b","b","b","b","b","b","b","b"]
+))
