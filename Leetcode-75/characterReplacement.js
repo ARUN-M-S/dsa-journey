a4130772d61658499b3c2db613f13493253baac4
ARUN-M-S <arunms96official@gmail.com>
2025-05-11 13:52:33 +0530

    character replace ment

--- Leetcode-75/characterReplacement.js
+++ Leetcode-75/characterReplacement.js
@@ -1,24 +1,25 @@
-function characterReplacement(s,k){
-    let count=0;
-    let char= s[0];
+function characterReplacement(s, k) {
+  let left = 0;
+  let maxFreq = 0;
+  let MaxLength = 0;
+  let map = new Map();
 
-    let maxCount =0;
-    let left =0;
+  for (let right = 0; right < s.length; right++) {
+    let char = s[right];
 
-    for(let right =0;right<s.length;right++){
-        if(s[right]==char){
-            count++;
-        }
-
-        while(count>k){
-            if(s[left] ==char){
-                count--;
-            }
-            left++;
-        }
-
-        maxCount = Math.max(maxCount,count);
+    map.set(char, (map.get(char) || 0) + 1);
+    maxFreq = Math.max(maxFreq, map.get(char));
+    let windoSize = right - left + 1;
+    while (windoSize - maxFreq > k) {
+      let leftChar = s[left];
+      map.set(leftChar, map.get(leftChar) - 1);
+      left++;
+      windoSize = right - left + 1;
     }
-    return maxCount;
 
+    MaxLength = Math.max(MaxLength, right - left + 1);
+  }
+  return MaxLength;
 }
+
+console.log(characterReplacement("AABABBA", 1));
