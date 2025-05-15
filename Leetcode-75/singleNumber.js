4edc7f3d2d28498a76a2c8a056e0b422894ec691
ARUN-M-S <arunms96official@gmail.com>
2025-05-15 19:45:32 +0530

    by bitwise operator

--- Leetcode-75/singleNumber.js
+++ Leetcode-75/singleNumber.js
@@ -1,13 +1,24 @@
+// function singleNumber(nums){
+// let freqCounter = {};
+// for(let num of nums){
+//     freqCounter[num] = (freqCounter[num]||0)+1;
+// }
+
+// for(let val of Object.keys(freqCounter)){
+//     if(freqCounter[val]==1) return val;
+// }
+
+// }
+
+
 function singleNumber(nums){
-let freqCounter = {};
-for(let num of nums){
-    freqCounter[num] = (freqCounter[num]||0)+1;
-}
-
-for(let val of Object.keys(freqCounter)){
-    if(freqCounter[val]==1) return val;
-}
-
-}
+    let result = 0;
+    for(let num of nums){
+        result^=num
+    }
+ 
+    return result
+    }
+singleNumber([1,1,2,4,4])
 
 singleNumber([1,1,2,4,4])
