75bd1abc0aa87c0bdbbe1a6e66b4f879aa4a2157
ARUN-M-S <arunms96official@gmail.com>
2025-05-16 16:34:31 +0530

    rotate

---
+++ Leetcode-75/rotate.js
@@ -0,0 +1,16 @@
+var rotate = function(nums, k) {
+   let reverse=(arr,left,right)=>{
+       while(left<right){
+           [arr[left],arr[right]]=[arr[right],arr[left]]
+           left++
+           right--
+       }
+   }
+   reverse(nums,0,k-1);
+   reverse(nums,k,nums.length-1);
+   reverse(nums,0,nums.length-1);
+   
+    return nums
+};
+
+console.log(rotate([1,2,3,4,5,6,7],3));
