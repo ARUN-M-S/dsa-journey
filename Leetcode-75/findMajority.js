8b277d4263902f52ee4f663b1f8a9aa213c45372
ARUN-M-S <arunms96official@gmail.com>
2025-05-18 22:33:50 +0530

    find Majority 1/3

--- Leetcode-75/findMajority.js
+++ Leetcode-75/findMajority.js
@@ -1,17 +1,38 @@
-function findMajority(arr) {
-    let result =[];
-    let freCouter = {};
-    
-    for(let nums of arr){
-        freCouter[nums] = (freCouter[nums]||0)+1;
-    }
-    
-    for(let key of Object.keys(freCouter)){
-        if(freCouter[key] >Math.floor(arr.length/3)){
-            result.push(Number(key))
+function findMajority(nums) {
+    let count1=0; let count2=0;
+    let majority1= null; let majority2=null;
+    let result=[]
+
+    for(let num of nums){
+        if(majority1==num){
+            count1++;
+        }else if(majority2==num){
+            count2++;
+        }else if(count1==0){
+            majority1=num;
+            count1++;
+        }else if(count2==0){
+            majority2=num;
+            count2++
+        }else{
+            count1--;
+            count2--;
         }
+
+       
     }
-    
+     count1=count2= 0;
+        for(let num of nums ){
+            if(majority1==num){
+            count1++;
+        }else if(majority2==num){
+            count2++
+        }
+        }
+
+        if(count1>Math.floor(nums.length/3))result.push(majority1)
+        if(count2>Math.floor(nums.length/3))result.push(majority2)
+
     return result;
   
 }
