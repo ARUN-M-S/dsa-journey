cf8986919fd2b205c9b6679697a49589e9aa82b1
ARUN-M-S <arunms96official@gmail.com>
2025-05-21 23:23:26 +0530

    four sum

--- Leetcode-75/fourSum.js
+++ Leetcode-75/fourSum.js
@@ -1,29 +1,33 @@
-var fourSum = function(nums, target) {
-    nums.sort((a,b)=>a-b);
-    let result =[]
-    for(let i =0;i<nums.length-2;i++){
-  let left = i+1;
-  let right = nums.length-1
+var fourSum = function (nums, target) {
+    nums.sort((a, b) => a - b);
+    let result = []
+    for (let i = 0; i < nums.length - 3; i++) {
+        if (i > 1 && nums[i] == nums[i - 1]) continue;
+        for (let j = i + 1; j < nums.length - 2; j++) {
+            if (j > 1 && nums[j] == nums[j - 1]) continue;
+            let left = j + 1;
+            let right = nums.length - 1
 
-  while(left<right){
-      let sum = nums[i]+nums[right]+nums[left]
-      if(sum==target){
-          result.push([nums[i],nums[left],nums[right]]);
+            while (left < right) {
+                let sum = nums[i] + nums[right] + nums[left] + nums[j]
+                if (sum == target) {
+                    result.push([nums[i], nums[left], nums[right], nums[j]]);
 
-          while(nums[right]==nums[right-1])right--;
-          while(nums[left]==nums[left+1])left++;
-          right--;
-          left++;
+                    while (nums[right] == nums[right - 1]) right--;
+                    while (nums[left] == nums[left + 1]) left++;
+                    right--;
+                    left++;
 
-      }else if(sum>0){
-          right--;
-      }else{
-          left++
-      }
-  }
+                } else if (sum > 0) {
+                    right--;
+                } else {
+                    left++
+                }
+            }
+        }
     }
     return result;
-    
+
 };
 
-console.log(fourSum([1,0,-1,0,-2,2],0));
+console.log(fourSum([1, 0, -1, 0, -2, 2], 0));
