42b41b4df3fa090ee845cc4c3d800f2fc1529fb5
ARUN-M-S <arunms96official@gmail.com>
2025-05-21 23:33:59 +0530

    four sum

--- Leetcode-75/fourSum.js
+++ Leetcode-75/fourSum.js
@@ -18,12 +18,49 @@ var fourSum = function (nums, target) {
                     right--;
                     left++;
 
-                } else if (sum > 0) {
+                } else if (sum > target) {
                     right--;
                 } else {
                     left++
                 }
+            }/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[][]}
+ */
+var fourSum = function (nums, target) {
+    nums.sort((a, b) => a - b);
+    let result = []
+    for (let i = 0; i < nums.length - 3; i++) {
+        if (i > 0 && nums[i] == nums[i - 1]) continue;
+
+        for (let j = i + 1; j < nums.length - 2; j++) {
+            if (j > i+1 && nums[j] == nums[j - 1]) continue;
+            let left = j + 1;
+            let right = nums.length - 1;
+
+
+            while (left < right) {
+                let sum = nums[left] + nums[right] + nums[i] + nums[j]
+
+                if (sum == target) {
+                    result.push([nums[left], nums[right], nums[i], nums[j]])
+                    while (left < right && nums[right] == nums[right - 1]) right--;
+                    while (left < right && nums[left] == nums[left + 1]) left++;
+
+                    right--;
+                    left++;
+                } else if (sum > target) {
+                    right--
+                } else {
+                    left++
+                }
             }
+        }
+    }
+    return result;
+
+};
         }
     }
     return result;
