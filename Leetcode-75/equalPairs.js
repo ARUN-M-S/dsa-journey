7b8ebe5cd04441e619d0d3d203d8f57ef213b71b
ARUN-M-S <arunms96official@gmail.com>
2025-05-24 22:57:22 +0530

    equal pairs

--- Leetcode-75/equalPairs.js
+++ Leetcode-75/equalPairs.js
@@ -1,18 +1,33 @@
 var equalPairs = function(grid) {
     let n = grid.length;
     let i =0;
-    let j =0
+    let j =0;
+    let count =0;
+    let map ={}
     while(i<n){
-        for(let j =0;j<n;j++){
-            console.log(grid[i][j]);           
-        }
-      
-        
-        i++;
-       
+      map[grid[i]] = (map[grid[i]]||0)+1;
+      i++;
     }
+    console.log(map)
+
+    while(j<n){
+        let columnArr=[]
+  for(let k=0;k<n;k++){
+    columnArr.push(grid[k][j])
+  }
+
+  if( map[columnArr.join(',')]){
+      count++;
+  } 
+       j++
+      }
+ return count;
 };
 
 
-let grid = [[3,2,1],[1,7,6],[2,7,7]];
+let grid = [
+            [3, 2, 1],
+            [1, 7, 6],
+            [2, 7, 7]
+        ];
 equalPairs(grid)
