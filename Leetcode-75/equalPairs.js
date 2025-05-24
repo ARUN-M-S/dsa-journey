e4e81caccbede1e68d81b1886301ab0601de0b16
ARUN-M-S <arunms96official@gmail.com>
2025-05-24 23:08:54 +0530

    chnaged the count value

--- Leetcode-75/equalPairs.js
+++ Leetcode-75/equalPairs.js
@@ -17,7 +17,7 @@ var equalPairs = function(grid) {
   }
 
   if( map[columnArr.join(',')]){
-      count++;
+      count+=map[columnArr.join(',')];
   } 
        j++
       }
@@ -25,9 +25,6 @@ var equalPairs = function(grid) {
 };
 
 
-let grid = [
-            [3, 2, 1],
-            [1, 7, 6],
-            [2, 7, 7]
-        ];
-equalPairs(grid)
+let grid = [[3,1,2,2],[1,4,4,5],[2,4,2,2],[2,4,2,2]];
+
+console.log( equalPairs(grid))
