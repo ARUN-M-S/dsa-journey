228a5920c2741e91ab97cd3dc737bd1d07527807
ARUN-M-S <arunms96official@gmail.com>
2025-06-09 21:27:41 +0530

    using set

--- Leetcode-75/duplicate.js
+++ Leetcode-75/duplicate.js
@@ -1,16 +1,14 @@
 function removeDuplicates(arr) {
     // your code here
-    let frqMap = new Map();
+    let seen = new Set()
     let result = [];
     for (let val of arr) {
-      if (!frqMap.get(val)) {
+      if (!seen.has(val)) {
+        seen.add(val)
         result.push(val);
       
       } 
-      frqMap.set(val,((frqMap.get(val)||0)+1))
-    // frqMap.set(val,val)
     }
-    console.log(frqMap);
     return result;
   }
   console.log( removeDuplicates([
