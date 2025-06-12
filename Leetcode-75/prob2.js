713a3a77f507fb878112269a4e66ac4b7a5ec282
ARUN-M-S <arunms96official@gmail.com>
2025-06-12 22:00:52 +0530

    namaste dsa prob 1

---
+++ Leetcode-75/prob2.js
@@ -0,0 +1,18 @@
+let arr = [2, 4, 8, 9, 7, 10];
+
+function secondLargest(arr) {
+    if (!Array.isArray(arr) || arr.length <= 1) return -1;
+    let largest = -Infinity;
+    let secondLarge = -Infinity;
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] > largest) {
+            secondLarge = largest;
+            largest = arr[i]
+        } else if (arr[i] > secondLarge) {
+            secondLarge = arr[i]
+        }
+    }
+    return secondLarge;
+}
+
+console.log(secondLargest(arr));
