8ea259b3431d8611ff5f67f82d494b2f35bd3cf9
ARUN-M-S <arunms96official@gmail.com>
2025-06-13 11:12:36 +0530

    problem 4: start pattren with number

--- namasteDSA/problem3.js
+++ namasteDSA/problem3.js
@@ -1,10 +1,10 @@
 function starPattern(n){
     for(let i=0;i<n;i++){
         let star='';
-        for(let j=0;j<=i;j++){
-            star+='*'
+        for(let j=1;j<=i+1;j++){
+            star+=j
         }
         console.log(star)
     }
 }
-starPattern(5)
+starPattern(5);
