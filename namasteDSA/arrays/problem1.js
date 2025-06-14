d562d17a005670ee01d2405f08cf1fc036767696
ARUN-M-S <arunms96official@gmail.com>
2025-06-14 11:45:17 +0530

    updated version

--- namasteDSA/arrays/problem1.js
+++ namasteDSA/arrays/problem1.js
@@ -2,10 +2,10 @@ let arr = [1, 2, 3, 3, 3, 5, 6, 7, 8, 9, 9, 10]
 
 function removeDuplicates(arr) {
     if (!Array.isArray(arr) || arr.length == 0) return -1;
-    let left = -1;
+    let left = 0;
     let right = 0;
     while (right < arr.length) {
-        if (arr[right] != arr[right + 1]) {
+        if (arr[right] > arr[left]) {
             left++;
             arr[left] = arr[right];
             right++;
