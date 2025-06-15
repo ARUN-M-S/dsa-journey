f68c14a56575a44fc6a61972d3c82a583fa51a2e
ARUN-M-S <arunms96official@gmail.com>
2025-06-15 12:31:00 +0530

    mising number

--- namasteDSA/arrays/problem8.js
+++ namasteDSA/arrays/problem8.js
@@ -9,4 +9,14 @@ function missingNumber(arr){
     return sum-sum2;
 }
 
+
+function xorMethod(arr){
+    let missng= arr.length;
+    for(let i =0;i<arr.length;i++){
+        missng^=i^arr[i]
+    }
+    return missng
+}
+
 console.log(missingNumber([0,1,3]))
+console.log(xorMethod([0,1,3]))
