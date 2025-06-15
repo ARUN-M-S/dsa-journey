5d578fdf05009fe7f24f9afeeb275e51f10cec8b
ARUN-M-S <arunms96official@gmail.com>
2025-06-15 17:12:41 +0530

    recusrion array in place

--- namasteDSA/recursion/problem3.js
+++ namasteDSA/recursion/problem3.js
@@ -1,10 +1,11 @@
 function recursionInplace(arr,start=0,end=arr.length-1){
-    if(start>=end) return;
+    if(start>=end) return arr;
     [arr[start],arr[end]]=[arr[end],arr[start]];
 
-    recursionInplace(arr,start+1,end-1)
+    return recursionInplace(arr,start+1,end-1)
 }
 let arr = [1,2,3,4,5]
-recursionInplace(arr)
+console.log(recursionInplace(arr))
+
 
 console.log(arr)
