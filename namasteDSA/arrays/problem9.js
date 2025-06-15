ac762bec94e0c52473ed26b176b7d2110042507f
ARUN-M-S <arunms96official@gmail.com>
2025-06-15 12:57:51 +0530

    new Approch for single number

--- namasteDSA/arrays/problem9.js
+++ namasteDSA/arrays/problem9.js
@@ -5,4 +5,17 @@ function singleNumber(arr){
     }
     return result
 }
+
+
+function secondMethod(arr){
+    let arrSum=0;
+    let max=0;
+    for(num of arr){
+        arrSum+=num;
+        max=Math.max(num,max)
+    }
+    let realSum  = max*(max+1);
+    return realSum-arrSum;
+}
 console.log(singleNumber([1,2,1,3,2,5,5,4,4]))
+console.log(secondMethod([1,2,1,3,2,5,5,4,4]))
