a72e0c5d12865ab35cedb7ac3147c307244da9ae
ARUN-M-S <arunms96official@gmail.com>
2025-06-15 11:20:16 +0530

    move zeros

---
+++ namasteDSA/arrays/problem6.js
@@ -0,0 +1,18 @@
+function moveZeros(arr) {
+    let count = 0;
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] != 0 && count > 0) {
+            let temp = arr[i];
+            arr[i] = 0;
+            arr[i - count] = temp;
+        } else if(arr[i]==0) {
+            count++ ;
+         }
+         console.log(arr)
+    }
+    return arr;
+}
+
+let arr1= [0,0,1,3,0,2,4];
+let arr2= [1,2,3,40,5,0,0,0]
+console.log(moveZeros(arr2))
