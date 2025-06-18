0f032c6cddc5b9ec656f27ecdbb44f74fccc3b76
ARUN-M-S <arunms96official@gmail.com>
2025-06-18 21:53:08 +0530

    Linked list

---
+++ namasteDSA/LinkedList/singlyLinkedList.js
@@ -0,0 +1,45 @@
+class Node{
+    constructor(val){
+        this.val=val;
+        this.next=null;
+    }
+}
+
+class SL{
+    constructor(){
+        this.head=null;
+        this.size=0;
+    }
+    push(val){
+        let newNode = new Node(val)
+        if(!this.head){
+            this.head = newNode;
+         
+        }else{
+            let temp = this.head;
+            while(temp.next){
+                temp = temp.next;
+            }
+            temp.next= newNode
+            
+           
+        }
+           this.size++
+        return this;
+    }
+    pop(){
+        if(!this.head) return undefined;
+        let temp = this.head;
+        let prev= temp;
+        while(temp.next){
+            prev= temp;
+            temp=temp.next;
+        }
+        prev.next=null;
+        this.size--;
+        if(this.size==0){
+            this.head=null
+        }
+        return temp
+    }
+}
