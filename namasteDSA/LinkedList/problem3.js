c346f38ce40aa72e079bd568ae8f3e59e784c993
ARUN-M-S <arunms96official@gmail.com>
2025-06-22 14:30:06 +0530

    intersection

---
+++ namasteDSA/LinkedList/problem3.js
@@ -0,0 +1,29 @@
+var getIntersectionNode = function (headA, headB) {
+    if (!headA || !headB) return undefined;
+
+    let set = new Set();
+    let temp1 = headA;
+    let temp2 = headB;
+    while (temp1 || temp2) {
+
+
+
+        if (temp1) {
+            if (set.has(temp1)) {
+                return temp1;
+            }
+            set.add(temp1);
+            temp1 = temp1.next;
+        } else if (temp2) {
+            if (set.has(temp2)) {
+                return temp2
+            }
+            set.add(temp2);
+            temp2 = temp2.next;
+            
+        }
+
+
+    }
+    return null
+};
