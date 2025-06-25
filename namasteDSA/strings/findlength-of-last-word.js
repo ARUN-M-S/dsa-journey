579165e124f2f7246e800242ebd37284e7e59894
ARUN-M-S <arunms96official@gmail.com>
2025-06-25 22:10:32 +0530

    single loop approch

--- namasteDSA/strings/findlength-of-last-word.js
+++ namasteDSA/strings/findlength-of-last-word.js
@@ -17,3 +17,19 @@ function findlength(s){
     }
     return count;
 }
+
+
+var lengthOfLastWord = function (s) {
+    let count = 0;
+    let n = s.length - 1;
+    while (n >= 0) {
+        if (s[n] !== " ") {
+            count++
+        }else if(count>0){
+            break
+        }
+        --n
+    }
+  
+    return count;
+};
