52ec763dd158032eb8a0ab5faaae5cdba02fad1b
ARUN-M-S <arunms96official@gmail.com>
2025-06-26 20:28:05 +0530

    optimised version

--- namasteDSA/strings/problem3.js
+++ namasteDSA/strings/problem3.js
@@ -20,3 +20,23 @@ var balancedStringSplit = function (s) {
 
     return result.length
 };
+
+
+var balancedStringSplit = function(s) {
+    let R=0;
+    let L=0;
+    let count=0;
+    for(let i =0;i<s.length;i++){
+        if(s[i]=='R'){
+            ++R
+        }else{
+            ++L
+        }
+        if(R==L){
+            count++;
+            R=0;
+            L=0
+        }
+    }
+    return count;
+};
