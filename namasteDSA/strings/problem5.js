834916beb92e258bbba06f0b8b04d4049f402980
ARUN-M-S <arunms96official@gmail.com>
2025-07-01 06:04:17 +0530

    palindrome

--- namasteDSA/strings/problem5.js
+++ namasteDSA/strings/problem5.js
@@ -5,3 +5,26 @@ var isPalindrome = function(s) {
     }
     return true;
 };
+
+
+
+var isPalindrome2 = function(s) {
+    s=s.toLowerCase();
+    let left =0;
+    let right=s.length-1;
+
+    while(left<right){
+        if(!s[left].match(/[a-z0-9]/i)){
+            left++
+        }else if(!s[right].match(/[a-z0-9]/i)){
+            right--
+        }else if(s[left]==s[right]){
+            left++;
+            right--
+        }else{
+            return false;
+        }
+    }
+    return true;
+    
+};
