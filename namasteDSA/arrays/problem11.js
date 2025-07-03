ab8bba991059093f7e109682e785546e1ff4e642
ARUN-M-S <arunms96official@gmail.com>
2025-07-03 21:04:48 +0530

    flatte array n nested time

---
+++ namasteDSA/arrays/problem11.js
@@ -0,0 +1,36 @@
+var flat = function (arr, n) {
+  if (n == 0) return arr;
+  let result = [];
+
+  function flatten(arr, count) {
+    for (let i = 0; i < arr.length; i++) {
+      if (Array.isArray(arr[i]) && count < n) {
+        flatten(arr[i], count + 1);
+      } else {
+        result.push(arr[i]);
+      }
+    }
+  }
+  flatten(arr, 0);
+  return result;
+};
+
+console.log(
+  flat([1, 2, 3, [4, 5, 6], [7, 8, [9, 10, 11], 12], [13, 14, 15]], 1)
+);
+
+// function flatten(arr,count) {
+//     for (let i = 0; i < arr.length; i++) {
+
+//         if (Array.isArray(arr[i]) && count<n) {
+
+//             flatten(arr[i],count+1);
+
+//         } else {
+//             result.push(arr[i])
+//         }
+//     }
+// }
+
+// flatten(arr,0);
+// return result;
