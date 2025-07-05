21df8deb2ead4bc2c4f60187a7a92c80850048d6
ARUN-M-S <arunms96official@gmail.com>
2025-07-05 20:57:33 +0530

    findTheDifference

---
+++ namasteDSA/strings/problem10.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {character}
+ */
+ var findTheDifference = function(s, t) {
+    if(s==t) return 'No difference'
+let sMap=Array(26).fill(0);
+let tMap=Array(26).fill(0);
+    for(let i=0;i<t.length;i++){
+       if(s[i]){
+       let sIndex= s[i] && s[i].charCodeAt() - 'a'.charCodeAt()
+        sMap[sIndex]++;
+
+       }
+       let tIndex = t[i] && t[i].charCodeAt()-'a'.charCodeAt();
+       tMap[tIndex]++;
+    }
+    for(let i =0;i<26;i++){
+        if(sMap[i]!=tMap[i]) return String.fromCharCode(i+'a'.charCodeAt())
+    }
+    return -1;
+};
