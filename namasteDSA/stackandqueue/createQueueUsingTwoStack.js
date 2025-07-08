b36dec1e0afea258786b2ec04e0a8ef51b01e5c5
ARUN-M-S <arunms96official@gmail.com>
2025-07-08 21:31:48 +0530

    optimised approch

--- namasteDSA/stackandqueue/createQueueUsingTwoStack.js
+++ namasteDSA/stackandqueue/createQueueUsingTwoStack.js
@@ -1,7 +1,73 @@
 
+// var MyQueue = function() {
+//     this.myStack1 =[];
+//     this.myStack2 =[];
+
+    
+// };
+
+// /** 
+//  * @param {number} x
+//  * @return {void}
+//  */
+// MyQueue.prototype.push = function(x) {
+//    this.myStack1.push(x)
+// };
+
+// /**
+//  * @return {number}
+//  */
+// MyQueue.prototype.pop = function() {
+//     let n = this.myStack1.length-1;
+//     for(let i = n;i>0;i--){
+//      this.myStack2.push(this.myStack1.pop())
+//     }
+//     let num =  this.myStack1.pop();
+//     n= this.myStack2.length-1;
+//      for(let i = n;i>=0;i--){
+//      this.myStack1.push(this.myStack2.pop())
+//     }
+//     return num;
+// };
+
+// /**
+//  * @return {number}
+//  */
+// MyQueue.prototype.peek = function() {
+//        let n = this.myStack1.length-1;
+//     for(let i = n;i>0;i--){
+//      this.myStack2.push(this.myStack1.pop())
+//     }
+//     let num =  this.myStack1.pop();
+//     this.myStack2.push((num));
+//     for(let i = n;i>=0;i--){
+//      this.myStack1.push(this.myStack2.pop())
+//     }
+
+//     return num;
+// };
+
+// /**
+//  * @return {boolean}
+//  */
+// MyQueue.prototype.empty = function() {
+//     return this.myStack1.length==0;
+// };
+
+// /** 
+//  * Your MyQueue object will be instantiated and called as such:
+//  * var obj = new MyQueue()
+//  * obj.push(x)
+//  * var param_2 = obj.pop()
+//  * var param_3 = obj.peek()
+//  * var param_4 = obj.empty()
+//  */
+
+
+
 var MyQueue = function() {
-    this.myStack1 =[];
-    this.myStack2 =[];
+    this.s1=[];
+    this.s2=[];
 
     
 };
@@ -11,47 +77,38 @@ var MyQueue = function() {
  * @return {void}
  */
 MyQueue.prototype.push = function(x) {
-   this.myStack1.push(x)
+    this.s1.push(x)
 };
 
 /**
  * @return {number}
  */
 MyQueue.prototype.pop = function() {
-    let n = this.myStack1.length-1;
-    for(let i = n;i>0;i--){
-     this.myStack2.push(this.myStack1.pop())
+    if(this.s2.length==0){
+        while(this.s1.length){
+            this.s2.push(this.s1.pop())
+        }
     }
-    let num =  this.myStack1.pop();
-    n= this.myStack2.length-1;
-     for(let i = n;i>=0;i--){
-     this.myStack1.push(this.myStack2.pop())
-    }
-    return num;
+    return this.s2.pop()
 };
 
 /**
  * @return {number}
  */
 MyQueue.prototype.peek = function() {
-       let n = this.myStack1.length-1;
-    for(let i = n;i>0;i--){
-     this.myStack2.push(this.myStack1.pop())
+      if(this.s2.length==0){
+        while(this.s1.length){
+            this.s2.push(this.s1.pop())
+        }
     }
-    let num =  this.myStack1.pop();
-    this.myStack2.push((num));
-    for(let i = n;i>=0;i--){
-     this.myStack1.push(this.myStack2.pop())
-    }
-
-    return num;
+    return this.s2[this.s2.length-1]
 };
 
 /**
  * @return {boolean}
  */
 MyQueue.prototype.empty = function() {
-    return this.myStack1.length==0;
+    return this.s1.length==0 && this.s2.length==0
 };
 
 /** 
