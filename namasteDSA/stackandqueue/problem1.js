8f502b078726aafbdf7f4f617a700015de95e6cf
ARUN-M-S <arunms96official@gmail.com>
2025-07-09 22:27:56 +0530

    swith out using stack

--- namasteDSA/stackandqueue/problem1.js
+++ namasteDSA/stackandqueue/problem1.js
@@ -3,21 +3,38 @@
  * @return {string}
  */
  var removeOuterParentheses = function(s) {
-    let stack =[];
+    // let stack =[];
+    // let ans="";
+    // for(let i =0;i<s.length;i++){
+    //     let pop= false;
+    //     if(s[i]=='('){
+    //         stack.push(s[i])
+    //     }else{
+    //         stack.pop()
+    //         pop=true;
+    //     }
+
+    //     let len = stack.length;
+    //     if(pop&&len>=1 || len>1){
+    //         ans+=s[i]
+    //     }
+    // }
+    // return ans;
     let ans="";
+    let count =0;
     for(let i =0;i<s.length;i++){
-        let pop= false;
+      
         if(s[i]=='('){
-            stack.push(s[i])
+            if(count>0)ans+=s[i]
+            count++
         }else{
-            stack.pop()
-            pop=true;
+            count--
+            if(count>0)ans+=s[i]
+            
+            
         }
 
-        let len = stack.length;
-        if(pop&&len>=1 || len>1){
-            ans+=s[i]
-        }
+      
     }
     return ans;
 };
