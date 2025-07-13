8fd6d14f8d5c15ec9942ecbbe66f4073afc0ffea
ARUN-M-S <arunms96official@gmail.com>
2025-07-13 13:34:11 +0530

    dailyTemperatures

--- namasteDSA/stackandqueue/dailyTemperatures.js
+++ namasteDSA/stackandqueue/dailyTemperatures.js
@@ -2,14 +2,15 @@ var dailyTemperatures = function(t) {
     let stack =[]
     let ans=Array(t.length).fill(0)
     for(let i=t.length-1;i>=0;i--){
-        let count=0
+     
         let n=stack.length-1
-        while(n>=0 && stack[n]<=t[i]){
-            count++;
+        while(n>=0 && t[stack[n]]<=t[i]){
+
             n--
+            stack.pop()
         }
-        n<0 ? ans[i]=0 :ans[i]=++count
-        stack.push(t[i])
+        n<0 ? ans[i]=0 :ans[i] = stack[n]-i
+        stack.push(i)
 
     }
     return ans
