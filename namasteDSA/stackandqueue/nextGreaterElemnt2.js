3e21867c6b74b1898ca1c0a3223e28f061bb03c9
ARUN-M-S <arunms96official@gmail.com>
2025-07-13 15:26:23 +0530

    with out multiply array

--- namasteDSA/stackandqueue/nextGreaterElemnt2.js
+++ namasteDSA/stackandqueue/nextGreaterElemnt2.js
@@ -12,3 +12,19 @@ var nextGreaterElements = function(nums) {
  
     return ans.slice(0,nums.length);
 };
+
+var nextGreaterElements2 = function(nums) {
+
+    let n = nums.length;
+    let stack = []
+    let ans= Array(n).fill(-1)
+    for(let i =(2*n)-1;i>=0;i--){
+        while(stack.length>0 && stack[stack.length-1]<=nums[i%n] ){
+            stack.pop()
+        }
+       stack.length>0 ? ans[i%n]=stack[stack.length-1] : '';
+       stack.push(nums[i%n])
+    }
+ 
+    return ans;
+};
