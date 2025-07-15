8621e7e3c7301579e445aa529e38e081aa142bc2
ARUN-M-S <arunms96official@gmail.com>
2025-07-15 23:21:58 +0530

    updated

--- namasteDSA/binarySearch/firstBadVesrion.js
+++ namasteDSA/binarySearch/firstBadVesrion.js
@@ -1,8 +1,5 @@
 var solution = function(isBadVersion) {
-    /**
-     * @param {integer} n Total versions
-     * @return {integer} The first bad version
-     */
+
     return function(n) {
         let left =1;
         let right = n;
