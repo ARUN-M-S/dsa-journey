deaa30c2c050ab4c11f2650a32390f1a458858a8
ARUN-M-S <arunms96official@gmail.com>
2025-07-17 21:55:37 +0530

    searchRange

---
+++ namasteDSA/binarySearch/findFirstAndLast.js
@@ -0,0 +1,29 @@
+var searchRange = function(nums, target) {
+    let l = 0;
+    let r= nums.length-1;
+    let arr =[-1,-1]
+    while(l<r){
+        let m = l+Math.floor((r-l)/2);
+
+        if(nums[m]<target){
+            l=m+1
+        }else{
+            r=m
+        }
+    }
+    if(nums[r]==target) arr[0] =r;
+     l = r;
+     r= nums.length-1;
+    while(l<r){
+        let m = l+Math.floor((r-l)/2);
+
+        if(nums[m]>target){
+            r=m
+        }else{
+            l=m+1
+        }
+    }
+    if(nums[r]==target) arr[1] =r;
+    return arr;
+    
+};
