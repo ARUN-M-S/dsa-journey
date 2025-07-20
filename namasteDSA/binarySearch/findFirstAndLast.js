b6819997f14337e80c76b0c62f128924024c8092
ARUN-M-S <arunms96official@gmail.com>
2025-07-20 23:15:22 +0530

    find first and last index

--- namasteDSA/binarySearch/findFirstAndLast.js
+++ namasteDSA/binarySearch/findFirstAndLast.js
@@ -1,31 +1,33 @@
-var searchRange = function(nums, target) {
+var searchRange = function (nums, target) {
     let l = 0;
-    let r= nums.length-1;
-    let arr =[-1,-1]
-    while(l<r){
-        let m = l+Math.floor((r-l)/2);
-        if(nums[m]==target){
-arr[0] = m
+    let r = nums.length - 1;
+    let arr = [-1, -1]
+    while (l < r) {
+        let m = l + Math.floor((r - l) / 2);
+        if (nums[m] == target) {
+            arr[0] = m;
+            r = m - 1;
         }
-        else if(nums[m]<target){
-            l=m+1
-        }else{
-            r=m
-        }
-    }
-    if(nums[r]==target) arr[0] =r;
-     l = r;
-     r= nums.length-1;
-    while(l<r){
-        let m = l+Math.floor((r-l)/2);
-
-        if(nums[m]>target){
-            r=m
-        }else{
-            l=m+1
+        else if (nums[m] < target) {
+            l = m + 1
+        } else {
+            r = m - 1
         }
     }
-    if(nums[r]==target) arr[1] =r;
+    l = 0;
+    r = nums.length - 1;
+    while (l < r) {
+        let m = l + Math.floor((r - l) / 2);
+        if (nums[m] == target) {
+            arr[1] = m;
+            l = m + 1;
+        }
+        if (nums[m] < target) {
+            l = m + 1
+        } else {
+            r = m - 1
+        }
+    }
     return arr;
-    
+
 };
