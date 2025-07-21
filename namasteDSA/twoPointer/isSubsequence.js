5a8309a72cc4ffd76f83ebf9aecefea325866885
ARUN-M-S <arunms96official@gmail.com>
2025-07-21 22:59:13 +0530

    code improment

--- namasteDSA/twoPointer/isSubsequence.js
+++ namasteDSA/twoPointer/isSubsequence.js
@@ -2,10 +2,7 @@ var isSubsequence = function(s, t) {
     let i =0;
     let j =0;
     while(i<s.length&&j<t.length){
-        if(s[i]==t[j]){
-            i++
-        }
-
+        if(s[i]==t[j])i++
     }
     return i==s.length ? true :false;;
     
