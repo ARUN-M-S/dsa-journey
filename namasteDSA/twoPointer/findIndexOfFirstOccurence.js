b69b6ea89e6ff5fa69eaa1211ff8013e895d9d69
ARUN-M-S <arunms96official@gmail.com>
2025-07-22 22:41:51 +0530

    code updations

--- namasteDSA/twoPointer/findIndexOfFirstOccurence.js
+++ namasteDSA/twoPointer/findIndexOfFirstOccurence.js
@@ -25,43 +25,43 @@
  * @param {string} needle
  * @return {number}
  */
- var strStr = function (haystack, needle) {
+var strStr = function (haystack, needle) {
     let m = haystack.length;
     let n = needle.length;
- let lps =[0];
- let i =0;
- let j =1;
- while(j<n){
-    if(needle[i]==needle[j]){
-        lps[j] =i+1;
-        j++;
-        i++;
-    }else{
-        if(i==0){
-            lps[j]=0;
+    let lps = [0];
+    let i = 0;
+    let j = 1;
+    while (j < n) {
+        if (needle[i] == needle[j]) {
+            lps[j] = i + 1;
+            j++;
+            i++;
+        } else {
+            if (i == 0) {
+                lps[j] = 0;
+                j++
+            } else {
+                i = lps[i - 1];
+            }
+        }
+
+    }
+    i = j = 0;
+    while (i < m) {
+        if (haystack[i] == needle[j]) {
+            i++;
             j++
-        }else{
-            i=lps[i-1];
+        } else {
+            if (j == 0) {
+                i++
+            } else {
+                j = lps[j - 1]
+            }
+        }
+
+        if (j == n) {
+            return i - n;
         }
     }
-  
- }
-i=j=0;
-while(i<m){
-      if(haystack[i]==needle[j]){
-        i++;
-        j++
-      }else{
-        if(j==0){
-            i++
-        }else{
-            j=lps[j-1]
-        }
-      }
-
-      if(j==n){
-        return i-n;
-      }
-}
-return -1;
+    return -1;
 };
