5a11f0dcafb9c641ec4b687504c4935684adb186
ARUN-M-S <arunms96official@gmail.com>
2025-07-22 21:02:21 +0530

    getIntersectionNode

---
+++ namasteDSA/twoPointer/getIntersectionNode.js
@@ -0,0 +1,11 @@
+var getIntersectionNode = function (headA, headB) {
+    if (!headA || !headB) return null;
+    let a = headA;
+    let b = headB;
+    while (true) {
+        if (a == b) return a;
+
+        a = a ? a.next : headB;
+        b = b ? b.next : headA;
+    }
+};
