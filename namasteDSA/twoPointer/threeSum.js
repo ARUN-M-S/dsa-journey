ad7263ee9c44edde00cbb775343731eb8882eb2d
ARUN-M-S <arunms96official@gmail.com>
2025-07-23 21:56:09 +0530

    logic is implemented

--- namasteDSA/twoPointer/threeSum.js
+++ namasteDSA/twoPointer/threeSum.js
@@ -1,20 +1,25 @@
 var threeSum = function(nums) {
-    nums.sort((a,b)=>a-b)
+    nums.sort((a,b)=>a-b);
+    let ans =[];
+    for(let i =0;i<nums.length;i++){
+        twoSum(nums,i,ans)
+    }
 };
 
 
 
 
- var twoSum = function (nums, target) {
-    let left = 0;
+ var twoSum = function (nums,x, ans) {
+    let left = x+1;
     let right = nums.length - 1;
 
     while (left < right) {
-        let sum = nums[left] + nums[right];
+        let sum = nums[left] + nums[right] +nums[x];
 
-        if (sum == target) {
-            return [left + 1, right + 1]
-        } else if (sum > target) {
+        if (sum == 0) {
+            ans.push([left+1,right+1,x])
+            left++; right--
+        } else if (sum > 0) {
             right--;
 
         } else {
