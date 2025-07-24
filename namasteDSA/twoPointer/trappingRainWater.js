0ff392e136505d6fa8d7e4df45243f77c971ac10
ARUN-M-S <arunms96official@gmail.com>
2025-07-24 21:13:11 +0530

    find right max

--- namasteDSA/twoPointer/trappingRainWater.js
+++ namasteDSA/twoPointer/trappingRainWater.js
@@ -5,4 +5,12 @@ var trap = function(arr) {
         maxL[i] =Math.max(maxL[i-1],arr[i])
     }
 
+    let maxR =[];
+    maxR[arr.length-1] = arr[arr.length-1]
+
+  for(let i=arr.length-2;i>=0;i--){
+      min =Math.max(maxR[i+1],arr[i])
+      maxR[i]=min
+  }
+
 }
