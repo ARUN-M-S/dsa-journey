bc2bb0f18a3a6ee0d57a6116408fea93f2b060fa
ARUN-M-S <arunms96official@gmail.com>
2025-07-24 23:00:12 +0530

    optimised

--- namasteDSA/twoPointer/lengthOfLongestSubwithoutDuplicate.js
+++ namasteDSA/twoPointer/lengthOfLongestSubwithoutDuplicate.js
@@ -3,7 +3,7 @@ var lengthOfLongestSubstring = function (s) {
     let i = 0;
     let j = 0;
     let map = {}
-    while (j < s.length) {
+    for(let j=0;j<s.length;j++){
         if (map[s[j]] >= i) {
             i = map[s[j]] + 1;
         } 
@@ -11,8 +11,11 @@ var lengthOfLongestSubstring = function (s) {
             max = Math.max(max, (j - i + 1));
         }
         map[s[j]] = j
-        j++;
     }
+    // while (j < s.length) {
+       
+    //     j++;
+    // }
     return max;
 
 };
