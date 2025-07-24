3c4bd813e26dab9bb858fd3417b0760506ab6f58
ARUN-M-S <arunms96official@gmail.com>
2025-07-24 21:38:07 +0530

    optimised

--- namasteDSA/twoPointer/trappingRainWater.js
+++ namasteDSA/twoPointer/trappingRainWater.js
@@ -1,19 +1,21 @@
-var trap = function(arr) {
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+ var trap = function(arr) {
+    let n = arr.length;
     let maxL =[arr[0]]
+    let maxR =[];
+      maxR[n-1] = arr[n-1]
 
     for(let i=1;i<arr.length;i++){
         maxL[i] =Math.max(maxL[i-1],arr[i])
+        maxR[n-i-1] =Math.max(maxR[n-i],arr[n-i-1])
     }
 
-    let maxR =[];
-    maxR[arr.length-1] = arr[arr.length-1]
+ 
 
-  for(let i=arr.length-2;i>=0;i--){
-      min =Math.max(maxR[i+1],arr[i])
-      maxR[i]=min
-  }
-
-  let sum =0;
+    let sum =0;
     for(let i =0;i<arr.length;i++){
         let min = Math.min(maxL[i],maxR[i])
         sum+= (min-arr[i])
@@ -21,4 +23,5 @@ var trap = function(arr) {
     return sum;
 
 
-}
+    
+};
