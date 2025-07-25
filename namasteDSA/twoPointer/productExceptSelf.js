02ed3bb10f26a0fe338d91663de527a76a7482eb
ARUN-M-S <arunms96official@gmail.com>
2025-07-25 20:40:04 +0530

    productExceptSelf

---
+++ namasteDSA/twoPointer/productExceptSelf.js
@@ -0,0 +1,16 @@
+var productExceptSelf = function (nums) {
+    let n = nums.length;
+    let result = Array(n).fill(1);
+
+    for (let i = 1; i < n; i++) {
+        result[i] = result[i - 1] * nums[i - 1]
+
+    }
+    let product = 1;
+    for (let i = n - 1; i >= 0; i--) {
+        result[i] = result[i] * product;
+        product *= nums[i]
+    }
+    return result;
+
+};
