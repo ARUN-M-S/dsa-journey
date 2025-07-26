f95b2d829c98bd7ea3ba6ddc0944b836d8ef9133
ARUN-M-S <arunms96official@gmail.com>
2025-07-26 14:55:42 +0530

    expandChar

---
+++ namasteDSA/strings/expandString.js
@@ -0,0 +1,27 @@
+
+function expandChar(data){
+    if(data.length<=0) return 'Please provide valid data';
+    let result =[];
+    let char = data[0];
+    let num ;
+    for(let i =1;i<data.length;i++){
+      if(isNaN(data[i])){
+        while(num>0){
+          result.push(char)
+          num--
+        }
+        char=data[i]
+      }else{
+        num = num ? num+data[i] :data[i]
+      }
+      
+    }
+    while(num>0){
+          result.push(char)
+          num--
+        }
+        
+        return result.join('')
+  }
+  
+  console.log(expandChar(x))
