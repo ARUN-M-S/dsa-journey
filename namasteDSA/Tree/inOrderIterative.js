d5d2283cf12df48d12c4d64412d4c63d22b116ae
ARUN-M-S <arunms96official@gmail.com>
2025-07-29 21:16:54 +0530

    inorderTraversal

---
+++ namasteDSA/Tree/inOrderIterative.js
@@ -0,0 +1,19 @@
+
+ var inorderTraversal = function(root) {
+
+    let ans =[];
+    let curr = root;
+    let stack =[];
+    while(curr ||stack.length){
+        while(curr){
+            stack.push(curr);
+            curr=curr.left;
+        }
+        curr = stack.pop()
+        ans.push(curr.val);
+        curr= curr.right
+    }
+ 
+    return ans
+    
+};
