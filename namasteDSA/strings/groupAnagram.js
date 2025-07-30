ff7e588db7852b7afabe9d09599ff2efb87afb06
ARUN-M-S <arunms96official@gmail.com>
2025-07-30 22:19:39 +0530

    group anagram

--- namasteDSA/strings/groupAnagram.js
+++ namasteDSA/strings/groupAnagram.js
@@ -1,46 +1,42 @@
-var groupAnagrams = function(strs) {
-    let result ={};
+var groupAnagrams = function (strs) {
+  let result = {};
 
-    for(let i =0;i<strs.length;i++){
-        let sorted = strs[i].split('').sort().join('')
-                if(result[sorted]){
-                    result[sorted].push(strs[i])
-                }else{
-                    result[sorted]=[strs[i]]
-                }
+  for (let i = 0; i < strs.length; i++) {
+    let sorted = strs[i].split("").sort().join("");
+    if (result[sorted]) {
+      result[sorted].push(strs[i]);
+    } else {
+      result[sorted] = [strs[i]];
     }
-    return Object.values(result)
-    
+  }
+  return Object.values(result);
 };
 
 // o(nMlogn) space;
 // o(m*n)
 
-function groupAnagrams2(strs){
-    let map ={};
-    for(let i =0;i<strs.length;i++){
-        let mapArray =Array(26).fill(0);
-        let s= strs[i];
-        for(let j =0;j<s.length;j++){
-            let index = s[j].charCodeAt() - 'a'.charCodeAt();
-            mapArray[index]++
-        }
-        let key =''
-        for(let k =0;k<26;k++){
-key=key+String.fromCharCode(k)+mapArray[k];
-
-        }
-
-        if(!map[key]){
-            map[key] = [s]
-        }else{
-            map[key].push(s)
-        }
-    
+function groupAnagrams2(strs) {
+  let map = {};
+  for (let i = 0; i < strs.length; i++) {
+    let mapArray = Array(26).fill(0);
+    let s = strs[i];
+    for (let j = 0; j < s.length; j++) {
+      let index = s[j].charCodeAt() - "a".charCodeAt();
+      mapArray[index]++;
+    }
+    let key = "";
+    for (let k = 0; k < 26; k++) {
+      key = key + String.fromCharCode(k) + mapArray[k];
     }
 
-    return [...Object.values(map)]
-    
+    if (!map[key]) {
+      map[key] = [s];
+    } else {
+      map[key].push(s);
+    }
+  }
+
+  return [...Object.values(map)];
 }
 
-console.log(groupAnagrams2(["eat","tea","tan","ate","nat","bat"]))
+console.log(groupAnagrams2(["eat", "tea", "tan", "ate", "nat", "bat"]));
