287989870957298cc3dd0b5a8653bfb086d4fd22
ARUN-M-S <arunms96official@gmail.com>
2025-07-30 22:18:25 +0530

    compress

--- namasteDSA/strings/compressString.js
+++ namasteDSA/strings/compressString.js
@@ -2,28 +2,26 @@
  * @param {character[]} chars
  * @return {number}
  */
- var compress = function(chars) {
-    
-    let current= chars[0];
-    let count =1;
-    let result = []
+var compress = function (chars) {
+  let current = chars[0];
+  let count = 1;
+  let result = [];
 
-    for(let i=1;i<chars.length;i++){
-       if(current ==chars[i]){
-        count++
-       }else{
-        result.push(current);
-         if(count>1)result.push(...count.toString());
-        current=chars[i];
-        count=1;
-       }
-      
+  for (let i = 1; i < chars.length; i++) {
+    if (current == chars[i]) {
+      count++;
+    } else {
+      result.push(current);
+      if (count > 1) result.push(...count.toString());
+      current = chars[i];
+      count = 1;
     }
-    result.push(current);
-    if (count > 1) result.push(...count.toString());
-   for (let i = 0; i < result.length; i++) {
-        chars[i] = result[i];
-    }
+  }
+  result.push(current);
+  if (count > 1) result.push(...count.toString());
+  for (let i = 0; i < result.length; i++) {
+    chars[i] = result[i];
+  }
 
-    return result.length;
+  return result.length;
 };
