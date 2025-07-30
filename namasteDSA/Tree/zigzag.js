aac43fc9da408ad0744ea53dd747f8e3487e8960
ARUN-M-S <arunms96official@gmail.com>
2025-07-30 22:12:05 +0530

    zigzag

--- namasteDSA/Tree/zigzag.js
+++ namasteDSA/Tree/zigzag.js
@@ -1,30 +1,25 @@
-var zigzagLevelOrder = function(root) {
-    if(!root) return []
-    let q=[root];
-    let ans =[];
-    let count=0;
-    while(q.length){
-        const lvlArr= [];
-        const lvlSize= q.length;
-        for(let i =0;i<lvlSize;i++){
-        let curr = q.shift();
-         
-         if(count%2==0){
-       lvlArr.push(curr.val);
-         }else{
-           lvlArr.unshift(curr.val);
-        
-         }
-          curr.left&&q.push(curr.left)
-        curr.right&&q.push(curr.right)
-        
-        }
-        count++;
-       
-        ans.push(lvlArr);
+var zigzagLevelOrder = function (root) {
+  if (!root) return [];
+  let q = [root];
+  let ans = [];
+  let count = 0;
+  while (q.length) {
+    const lvlArr = [];
+    const lvlSize = q.length;
+    for (let i = 0; i < lvlSize; i++) {
+      let curr = q.shift();
 
+      if (count % 2 == 0) {
+        lvlArr.push(curr.val);
+      } else {
+        lvlArr.unshift(curr.val);
+      }
+      curr.left && q.push(curr.left);
+      curr.right && q.push(curr.right);
     }
-    return ans;
-    
+    count++;
 
+    ans.push(lvlArr);
+  }
+  return ans;
 };
