cb21ad38a0862e4cbf05316bae6b1e105b6bb312
ARUN-M-S <arunms96official@gmail.com>
2025-07-30 22:13:06 +0530

    inorder

--- namasteDSA/Tree/inOrderIterative.js
+++ namasteDSA/Tree/inOrderIterative.js
@@ -1,19 +1,16 @@
-
- var inorderTraversal = function(root) {
-
-    let ans =[];
-    let curr = root;
-    let stack =[];
-    while(curr ||stack.length){
-        while(curr){
-            stack.push(curr);
-            curr=curr.left;
-        }
-        curr = stack.pop()
-        ans.push(curr.val);
-        curr= curr.right
+var inorderTraversal = function (root) {
+  let ans = [];
+  let curr = root;
+  let stack = [];
+  while (curr || stack.length) {
+    while (curr) {
+      stack.push(curr);
+      curr = curr.left;
     }
- 
-    return ans
-    
+    curr = stack.pop();
+    ans.push(curr.val);
+    curr = curr.right;
+  }
+
+  return ans;
 };
