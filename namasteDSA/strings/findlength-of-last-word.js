4d8de414312ed504003219cc1f829f21d1b8aed0
ARUN-M-S <arunms96official@gmail.com>
2025-07-30 22:19:16 +0530

    findLength

--- namasteDSA/strings/findlength-of-last-word.js
+++ namasteDSA/strings/findlength-of-last-word.js
@@ -1,35 +1,34 @@
-function findlength(s){
-    let n = s.length-1;
-    while(n){
-        if(s[n]==" "){
-            n--
-        }else{
-            break
-        }
+function findlength(s) {
+  let n = s.length - 1;
+  while (n) {
+    if (s[n] == " ") {
+      n--;
+    } else {
+      break;
     }
-    let count=0;
-    for(let i=n;i>=0;i++){
-        if(s[i]!=" "){
-            count++
-        }else{
-            return count;
-        }
+  }
+  let count = 0;
+  for (let i = n; i >= 0; i++) {
+    if (s[i] != " ") {
+      count++;
+    } else {
+      return count;
     }
-    return count;
+  }
+  return count;
 }
 
-
 var lengthOfLastWord = function (s) {
-    let count = 0;
-    let n = s.length - 1;
-    while (n >= 0) {
-        if (s[n] !== " ") {
-            count++
-        }else if(count>0){
-            break
-        }
-        --n
+  let count = 0;
+  let n = s.length - 1;
+  while (n >= 0) {
+    if (s[n] !== " ") {
+      count++;
+    } else if (count > 0) {
+      break;
     }
-  
-    return count;
+    --n;
+  }
+
+  return count;
 };
