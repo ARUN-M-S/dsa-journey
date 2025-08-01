a564dced3c246349dc28441b382b118db3bf3046
ARUN-M-S <arunms96official@gmail.com>
2025-08-01 23:12:11 +0530

    alternative approch

--- namasteDSA/Tree/pathSum.js
+++ namasteDSA/Tree/pathSum.js
@@ -1,17 +1,31 @@
-var hasPathSum = function (root, targetSum) {
-  if (!root) return false;
-  let ans = false;
-  function travers(curr, currSum) {
-    let sum = currSum + curr.val;
-    if (!curr.left && !curr.right) {
-      if (sum == targetSum) {
-        ans = ans || true;
-      }
-    }
 
-    curr.left && travers(curr.left, sum);
-    curr.right && travers(curr.right, sum);
-  }
-  travers(root, 0);
-  return ans;
+ var hasPathSum = function(root, targetSum) {
+
+    if(!root) return false;
+    // let ans = false;
+    // function travers(curr,currSum){
+    //     let sum = currSum+curr.val;
+    //     if(!curr.left && !curr.right){
+    //         if(sum ==targetSum){
+    //         ans = ans || true;
+    //         }
+          
+    //     }
+
+    //    curr.left&& travers(curr.left,sum)
+    //    curr.right&& travers(curr.right,sum)
+
+    // }
+    // travers(root,0)
+    // return ans
+    // ;
+
+    if(!root.left && !root.right){
+        if(targetSum == root.val){
+            return true;
+        }
+    }
+        return hasPathSum(root.left, targetSum-root.val) || hasPathSum(root.right, targetSum-root.val) 
+
+     
 };
