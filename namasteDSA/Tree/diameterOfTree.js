7b1dfcebe10006f941e6dfca9560cff4b5c86b73
ARUN-M-S <arunms96official@gmail.com>
2025-08-02 23:44:18 +0530

    depth

--- namasteDSA/Tree/diameterOfTree.js
+++ namasteDSA/Tree/diameterOfTree.js
@@ -1,18 +1,18 @@
-var diameterOfBinaryTree = function(root) {
-    if(!root) return 0;
+var diameterOfBinaryTree = function (root) {
+    if (!root) return 0;
     let maxLength = 0;
 
-    let level = (curr)=>{
+    let level = (curr) => {
 
-        if(!curr) return 0;
+        if (!curr) return 0;
         let left = level(curr.left)
         let right = level(curr.right)
-        let sum = left +right;
-        maxLength = Math.max(maxLength,sum);
-        return 1+Math.max(left,right)
+        let sum = left + right;
+        maxLength = Math.max(maxLength, sum);
+        return 1 + Math.max(left, right)
 
     }
     level(root);
     return maxLength
-    
+
 };
