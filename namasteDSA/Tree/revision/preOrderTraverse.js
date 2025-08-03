4a76b7e0ca4b8c7ee4c3c02a966aa42a5542f2e5
ARUN-M-S <arunms96official@gmail.com>
2025-08-03 16:11:20 +0530

    revision day- Preorder traverse

---
+++ namasteDSA/Tree/revision/preOrderTraverse.js
@@ -0,0 +1,15 @@
+var preorderTraversal = function(root) {
+    if(!root) return [];
+    let ans =[];
+    function travers(curr){
+        if(!curr) return;
+        ans.push(curr.val);
+        curr.left && travers(curr.left)
+        curr.right && travers(curr.right)
+
+    };
+
+    travers(root)
+    return ans;
+    
+};
