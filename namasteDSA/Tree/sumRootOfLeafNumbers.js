cee8824dfc1352e9866c0b9e0e635175e785e643
ARUN-M-S <arunms96official@gmail.com>
2025-08-08 23:20:38 +0530

    sumNumbers

---
+++ namasteDSA/Tree/sumRootOfLeafNumbers.js
@@ -0,0 +1,15 @@
+var sumNumbers = function(root) {
+
+    let travers = (curr,currSum)=>{
+     if(!curr) return 0;
+     currSum = currSum*10 +curr.val;
+     if(!curr.left && !curr.right) return currSum;
+  
+     return travers(curr.left,currSum) + travers(curr.right,currSum)
+     
+  
+    }
+    return travers(root,0);
+  
+      
+  };
