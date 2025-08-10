e62231d4c14094e705ec4c633bb077f8e34de71f
ARUN-M-S <arunms96official@gmail.com>
2025-08-10 20:46:36 +0530

    revision

--- namasteDSA/Tree/maxDepth.js
+++ namasteDSA/Tree/maxDepth.js
@@ -1,20 +1,20 @@
 
- var maxDepth = function(root) {
-    if(!root) return 0;
- 
+var maxDepth = function (root) {
+    if (!root) return 0;
+
     let maxLvl = 0;
 
-   
-    function level(curr,lvl){
-        if(!curr)return;
-        maxLvl = Math.max(maxLvl,lvl)
-        curr.left && level(curr.left,lvl+1)
-         curr.right && level(curr.right,lvl+1)
-        
+
+    function level(curr, lvl) {
+        if (!curr) return;
+        maxLvl = Math.max(maxLvl, lvl)
+        curr.left && level(curr.left, lvl + 1)
+        curr.right && level(curr.right, lvl + 1)
+
     }
 
-    level(root,0);
-    return maxLvl+1;
+    level(root, 0);
+    return maxLvl + 1;
 
-    
+
 };
