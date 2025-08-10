ed92205e3eb0a59c949a5252cf92a5194caa7e1f
ARUN-M-S <arunms96official@gmail.com>
2025-08-10 20:47:08 +0530

    revision

--- namasteDSA/Tree/minDepth.js
+++ namasteDSA/Tree/minDepth.js
@@ -1,9 +1,9 @@
-var minDepth = function(root) {
-    if(!root) return 0;
+var minDepth = function (root) {
+    if (!root) return 0;
 
-    if(!root.left) return 1+ minDepth(root.right)
-     if(!root.right) return 1+ minDepth(root.left)
+    if (!root.left) return 1 + minDepth(root.right)
+    if (!root.right) return 1 + minDepth(root.left)
 
-     return 1+ Math.min(minDepth(root.right),minDepth(root.left))
-    
+    return 1 + Math.min(minDepth(root.right), minDepth(root.left))
+
 };
