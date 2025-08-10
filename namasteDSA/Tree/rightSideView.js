9dc93e11c90cc05bc8c6f84c9161f99414e68de4
ARUN-M-S <arunms96official@gmail.com>
2025-08-10 20:48:40 +0530

    revision

--- namasteDSA/Tree/rightSideView.js
+++ namasteDSA/Tree/rightSideView.js
@@ -1,20 +1,20 @@
-var rightSideView = function(root) {
-    if(!root) return []
-    let ans=[];
+var rightSideView = function (root) {
+    if (!root) return []
+    let ans = [];
     let q = [root];
-    while(q.length){
+    while (q.length) {
         let size = q.length;
-        for(let i =0;i<size;i++){
+        for (let i = 0; i < size; i++) {
             let curr = q.shift();
-            if(i ==0){
+            if (i == 0) {
                 ans.push(curr.val)
             }
-             curr.right && q.push(curr.right)
+            curr.right && q.push(curr.right)
             curr.left && q.push(curr.left)
-           
+
 
         }
     }
     return ans;
-    
+
 };
