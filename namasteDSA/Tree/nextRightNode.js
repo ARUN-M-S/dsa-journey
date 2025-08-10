c19ac8fe6744cfeb4d32bbf2373fb8c63087cc3f
ARUN-M-S <arunms96official@gmail.com>
2025-08-10 20:47:23 +0530

    revision

--- namasteDSA/Tree/nextRightNode.js
+++ namasteDSA/Tree/nextRightNode.js
@@ -1,5 +1,5 @@
 var connect = function (root) {
-    if(!root) return root
+    if (!root) return root
     let ans = [];
     let q = [root]
 
@@ -9,12 +9,12 @@ var connect = function (root) {
 
         for (let i = 0; i < size; i++) {
             let curr = q.shift();
-            if(i<size-1){
-              curr.next=q[0]
-            }else{
-                curr.next=null
+            if (i < size - 1) {
+                curr.next = q[0]
+            } else {
+                curr.next = null
             }
-           
+
             curr.left && q.push(curr.left)
             curr.right && q.push(curr.right)
         }
@@ -26,13 +26,13 @@ var connect = function (root) {
 
 };
 
-let traverse = (curr)=>{
-    if(curr.left) { curr.left.next =curr.right;}
-    if(curr.right && curr.next){
+let traverse = (curr) => {
+    if (curr.left) { curr.left.next = curr.right; }
+    if (curr.right && curr.next) {
         curr.right.next = curr.next.left;
     }
-     curr.left&& traverse(curr.left)
-       curr.right&& traverse(curr.right)
+    curr.left && traverse(curr.left)
+    curr.right && traverse(curr.right)
 }
 traverse(root);
 // return root
