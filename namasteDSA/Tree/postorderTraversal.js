f0c1ef95d26a7ffa9bd08c249ee4d8d0f98e688c
ARUN-M-S <arunms96official@gmail.com>
2025-08-10 20:47:52 +0530

    revision

--- namasteDSA/Tree/postorderTraversal.js
+++ namasteDSA/Tree/postorderTraversal.js
@@ -1,12 +1,12 @@
 var postorderTraversal = function (root) {
-  let ans = [];
-  function traversal(curr) {
-    if (!curr) return;
-    traversal(curr?.left);
+    let ans = [];
+    function traversal(curr) {
+        if (!curr) return;
+        traversal(curr?.left);
 
-    traversal(curr?.right);
-    ans.push(curr?.val);
-  }
-  traversal(root);
-  return ans;
+        traversal(curr?.right);
+        ans.push(curr?.val);
+    }
+    traversal(root);
+    return ans;
 };
