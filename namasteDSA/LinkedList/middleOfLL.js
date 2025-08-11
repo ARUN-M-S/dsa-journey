ed650023fb94ffa62964aba55e2f8d1b37a03a22
ARUN-M-S <arunms96official@gmail.com>
2025-08-11 22:13:10 +0530

    optimisation

--- namasteDSA/LinkedList/middleOfLL.js
+++ namasteDSA/LinkedList/middleOfLL.js
@@ -1,13 +1,13 @@
-var middleNode = function(head) {
+var middleNode = function (head) {
     let fast = head;
-      let slow=head;
-   
+    let slow = head;
 
-      while(fast!=null && fast.next!=null){
-          slow=slow.next
-          fast=fast.next.next;
-        
-         
-      }
-      return slow;
+
+    while (fast != null && fast.next != null) {
+        slow = slow.next
+        fast = fast.next.next;
+
+
+    }
+    return slow;
 };
