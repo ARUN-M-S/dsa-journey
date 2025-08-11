bdca64aed02a09f678ddf3e7a3906b623895fd39
ARUN-M-S <arunms96official@gmail.com>
2025-08-11 22:07:10 +0530

    Saerch in BST

---
+++ namasteDSA/BST/searchInBST.js
@@ -0,0 +1,19 @@
+var searchBST = function(root, val) {
+    let ans = null
+  let traverse = (curr)=>{
+      if(!curr)return null
+      if(curr.val==val){
+          ans = curr
+      }else{
+          if(val <curr.val){
+              traverse(curr.left)
+          }else{
+              traverse(curr.right)
+          }
+      }
+  }
+  traverse(root)
+  
+  return ans
+      
+  };
