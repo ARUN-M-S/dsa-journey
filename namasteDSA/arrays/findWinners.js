3c7e1f1b825d319dfc2a7ba96c8cf6485938fbdf
ARUN-M-S <arunms96official@gmail.com>
2025-08-15 21:24:32 +0530

    find winnwers another approch

--- namasteDSA/arrays/findWinners.js
+++ namasteDSA/arrays/findWinners.js
@@ -24,3 +24,27 @@ var findWinners = function(matches) {
     })
     return ans
 };
+
+
+var findWinners = function(matches) {
+    let lostMap = {};
+
+ for (let [winner, loser] of matches) {
+     lostMap[winner] = lostMap[winner] || 0; // ensure winner is in map with 0 losses
+     lostMap[loser] = (lostMap[loser] || 0) + 1;
+ }
+  let zeroLoss = [];
+ let oneLoss = [];
+
+ for (let player in lostMap) {
+     if (lostMap[player] === 0) {
+         zeroLoss.push(Number(player));
+     } else if (lostMap[player] === 1) {
+         oneLoss.push(Number(player));
+     }
+ }
+ zeroLoss.sort((a, b) => a - b);
+ oneLoss.sort((a, b) => a - b);
+
+ return [zeroLoss, oneLoss];
+};
