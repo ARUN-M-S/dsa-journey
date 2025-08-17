b63fdb2a62a62d35da982d1a4da2a89eca959d56
ARUN-M-S <arunms96official@gmail.com>
2025-08-17 20:01:00 +0530

    build Tree

---
+++ namasteDSA/Tree/buildTree.js
@@ -0,0 +1,20 @@
+var buildTree = function(preorder, inorder) {
+    let preIndex = 0;
+    let helper = (sIdx,eIdx)=>{
+        if(preIndex >= preorder.length || sIdx>eIdx) return null
+
+        let root = new TreeNode(preorder[preIndex]);
+       if (sIdx === eIdx) {
+            preIndex++;
+            return root;
+        }
+         let leftIno = inorder.indexOf(preorder[preIndex]);
+         preIndex++;
+         root.left = helper(sIdx,leftIno-1);
+         root.right = helper(leftIno+1,eIdx);
+         return root;
+
+    };
+    return helper(0,preorder.length-1)
+    
+};
