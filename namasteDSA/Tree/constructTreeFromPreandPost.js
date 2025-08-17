cdca21f86c1ddecf242135179194161b4ddf755b
ARUN-M-S <arunms96official@gmail.com>
2025-08-17 19:46:19 +0530

    construct tree

--- namasteDSA/Tree/constructTreeFromPreandPost.js
+++ namasteDSA/Tree/constructTreeFromPreandPost.js
@@ -1,19 +1,19 @@
-var constructFromPrePost = function(preorder, postorder) {
-    if(preorder.length !==postorder.length) return false;
+var constructFromPrePost = function (preorder, postorder) {
+    if (preorder.length !== postorder.length) return false;
     let preIndex = 0;
-    function constructTree(postStart,postEnd){
-           if (preIndex >= preorder.length || postStart > postEnd) return null;
+    function constructTree(postStart, postEnd) {
+        if (preIndex >= preorder.length || postStart > postEnd) return null;
 
         let root = new TreeNode(preorder[preIndex]);
         preIndex++;
         if (postStart === postEnd || preIndex > preorder.length) return root;
         let leftPost = postorder.indexOf(preorder[preIndex])
-        root.left = constructTree(postStart,leftPost)
-        root.right = constructTree(leftPost+1,postEnd-1)
+        root.left = constructTree(postStart, leftPost)
+        root.right = constructTree(leftPost + 1, postEnd - 1)
 
         return root
 
     }
-   return  constructTree(preIndex,postorder.length-1)
-    
+    return constructTree(preIndex, postorder.length - 1)
+
 };
