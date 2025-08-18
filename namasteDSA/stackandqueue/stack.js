a6c304e6eca818dbd6b9f17dd0d51912838bbc2e
ARUN-M-S <arunms96official@gmail.com>
2025-08-18 23:21:26 +0530

    stack

--- namasteDSA/stackandqueue/stack.js
+++ namasteDSA/stackandqueue/stack.js
@@ -1,36 +1,36 @@
 class Stack {
     constructor() {
         // Initialize your stack
-        this.item =[]
+        this.item = []
     }
-    
+
     push(element) {
         // Add element to the top
         this.item.push(element);
         return this.size()
     }
-    
-     pop() {
-    if (this.isEmpty()) return undefined;
-    return this.item.pop();
-  }
 
-  // Peek top element
-  peek() {
-    if (this.isEmpty()) return undefined;
-    return this.item[this.item.length - 1];
-  }
-    
+    pop() {
+        if (this.isEmpty()) return undefined;
+        return this.item.pop();
+    }
+
+    // Peek top element
+    peek() {
+        if (this.isEmpty()) return undefined;
+        return this.item[this.item.length - 1];
+    }
+
     isEmpty() {
         // Check if stack is empty
-        return this.item.length ==0
+        return this.item.length == 0
     }
-    
+
     size() {
         // Return number of elements
         return this.item.length;
     }
-    
+
     clear() {
         // Remove all elements
         this.item = []
