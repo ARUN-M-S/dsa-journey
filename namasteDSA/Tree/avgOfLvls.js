58062fd5c5e535cca9ee88fb720a625720cafa90
ARUN-M-S <arunms96official@gmail.com>
2025-08-18 23:11:22 +0530

    avg of levels optimisation

--- namasteDSA/Tree/avgOfLvls.js
+++ namasteDSA/Tree/avgOfLvls.js
@@ -5,12 +5,13 @@ var averageOfLevels = function (root) {
         let size = q.length;
         let sum = 0;
         for (let i = 0; i < size; i++) {
-            let curr = q.shift();
+            let curr = q[i];
             sum += curr.val;
             curr.left && q.push(curr.left)
             curr.right && q.push(curr.right)
         }
-        avg.push(sum / size)
+        avg.push(sum / size);
+        q=q.slice(size)
     }
     return avg
 
