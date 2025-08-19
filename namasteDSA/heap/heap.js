8a699e2fd246e08dfe9ffbdbb52eabf367ccc98a
ARUN-M-S <arunms96official@gmail.com>
2025-08-19 22:43:37 +0530

    heap created

--- namasteDSA/heap/heap.js
+++ namasteDSA/heap/heap.js
@@ -1,21 +1,41 @@
-class MinHeap{
-    constructor(){
-        this.heap = [5,10,20,30]
+class MinHeap {
+    constructor() {
+        this.heap = [5, 10, 20, 30]
     }
-    getLeftChildIndex(i){
-        return 2*i+1
-    }
-
-    getRightChildIndex(i){
-        return 2*i+2
+    getLeftChildIndex(i) {
+        return 2 * i + 1
     }
 
-    getParentIndex(i){
-        return Math.floor((i-1)/2)
+    getRightChildIndex(i) {
+        return 2 * i + 2
     }
 
-    insert(val){
+    getParentIndex(i) {
+        return Math.floor((i - 1) / 2)
+    }
 
+    insert(val) {
+        this.heap.push(val);
+        let index = this.heap.length - 1;
+        this.heapifyUp(index)
+
+
+    }
+    heapifyUp(i) {
+        while (i > 0) {
+            let parentIndex = this.getParentIndex(i);
+            if (this.heap[parentIndex] > this.heap[i]) {
+                [this.heap[parentIndex], this.heap[i]] = [this.heap[i], this.heap[parentIndex]];
+                i = parentIndex
+            }else{
+                break;
+            }
+        }
     }
 
 }
+
+let heap = new MinHeap();
+heap.insert(7)
+
+console.log(heap.heap)
