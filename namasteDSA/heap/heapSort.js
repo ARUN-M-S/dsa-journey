918422002641b1c6300c91cd6f9c606c6e1ac045
ARUN-M-S <arunms96official@gmail.com>
2025-08-20 23:31:35 +0530

    heap sort

--- namasteDSA/heap/heapSort.js
+++ namasteDSA/heap/heapSort.js
@@ -3,25 +3,30 @@ function heapSort(arr){
     let  n = arr.length;
 
     for(let i =n-1;i>=0;i--){
-heapifyDown(arr,i)
+      heapifyDown(arr,i,n)
     }
+    for(let i =n-1;i>0;i--){
+      [arr[i],arr[0]]  =  [arr[0],arr[i]] ;
+      heapifyDown(arr,0,i)
+    }
+    return arr;
 
 }
 
-function heapifyDown(arr,i){
+function heapifyDown(arr,i,n){
     let largest = i;
     let left = (2*i)+1;
     let right = (2*i)+2;
-    if(arr[left]>arr[largest]){
+    if(left <n && arr[left]>arr[largest]){
         largest = left
     }
-    if(arr[right]> arr[largest]){
+    if(right < n && arr[right]> arr[largest]){
         largest= right
     }
 
     if(i!==largest){
         [arr[largest],arr[i]] = [arr[i],arr[largest]]
-        heapifyDown(arr,largest)
+        heapifyDown(arr,largest,n)
 
     }
 }
