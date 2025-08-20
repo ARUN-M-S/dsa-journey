c67c823fcaa1205e059ad36cca17aa6e4d730b76
ARUN-M-S <arunms96official@gmail.com>
2025-08-20 21:45:09 +0530

    heapifydown

--- namasteDSA/heap/heap.js
+++ namasteDSA/heap/heap.js
@@ -19,6 +19,11 @@ class MinHeap {
         let index = this.heap.length - 1;
         this.heapifyUp(index)
     }
+    extract(){
+        this.heap[0] = this.heap[this.heap.length-1];
+        this.heap.pop();
+        this.heapifyDown(0)
+    }
     heapifyUp(i) {
         while (i > 0) {
             let parentIndex = this.getParentIndex(i);
@@ -30,10 +35,27 @@ class MinHeap {
             }
         }
     }
+    heapifyDown(i){
+        while(i<this.heap.length){
+            let leftIndex = this.getLeftChildIndex(i);
+            let rightIndex = this.getRightChildIndex(i);
+            if(this.heap[leftIndex]<this.heap[rightIndex]){
+                [this.heap[leftIndex],this.heap[i]] = [this.heap[i],this.heap[leftIndex]]
+                i = leftIndex
+            }else if(this.heap[rightIndex]<this.heap[leftIndex]){
+                [this.heap[rightIndex],this.heap[i]] = [this.heap[i],this.heap[rightIndex]]
+                i=rightIndex
+            }else{
+                break;
+            }
+        }
+    }
 }
 
 let heap = new MinHeap();
 heap.insert(7)
 heap.insert(3)
 
+console.log(heap.heap)
+heap.extract()
 console.log(heap.heap)
