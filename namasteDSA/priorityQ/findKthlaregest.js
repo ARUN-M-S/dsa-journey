859b8fae7d4118403d4d9d783239cf8a732d3a35
ARUN-M-S <arunms96official@gmail.com>
2025-08-22 22:41:56 +0530

    kth largest using heap or proriry queue

--- namasteDSA/priorityQ/findKthlaregest.js
+++ namasteDSA/priorityQ/findKthlaregest.js
@@ -2,8 +2,6 @@ var findKthLargest = function(nums, k) {
     return nums.sort((a,b)=>b-a)[k-1]
     
 };
-
-
 var findKthLargest = function(nums, k) {
     let pq = new MinPriorityQueue();
     for(let i =0;i<nums.length;i++){
