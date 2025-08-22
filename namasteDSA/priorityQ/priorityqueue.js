998b0b76b0d986f2b887351b49cd01241e5dad36
ARUN-M-S <arunms96official@gmail.com>
2025-08-22 22:42:41 +0530

    prority queue

--- namasteDSA/priorityQ/priorityqueue.js
+++ namasteDSA/priorityQ/priorityqueue.js
@@ -1,30 +1,30 @@
-class PriorityQueue{
-    constructor(){
-        this.queue= []
+class PriorityQueue {
+    constructor() {
+        this.queue = []
     }
 
-    enque(val,prority){
-        this.queue.push({val,prority});
-        this.queue.sort((a,b)=>b.prority -a.prority)
+    enque(val, prority) {
+        this.queue.push({ val, prority });
+        this.queue.sort((a, b) => b.prority - a.prority)
     }
-    dequeue(){
+    dequeue() {
         return this.queue.shift()
     }
-    peek(){
+    peek() {
         return this.queue[0]
     }
-    isEmpty(){
-        return this.queue.length ==0
+    isEmpty() {
+        return this.queue.length == 0
     }
-    size(){
+    size() {
         return this.queue.length;
     }
 }
 let pq = new PriorityQueue();
-pq.enque("arun",1)
-pq.enque("arun2",2)
-pq.enque("arun10",10)
-pq.enque("arun6",6)
+pq.enque("arun", 1)
+pq.enque("arun2", 2)
+pq.enque("arun10", 10)
+pq.enque("arun6", 6)
 
 console.log(pq.dequeue())
 
