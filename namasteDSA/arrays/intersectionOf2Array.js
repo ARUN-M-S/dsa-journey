a1ec50296bafa704d5c8c8d9f95f9ab4a1421345
ARUN-M-S <arunms96official@gmail.com>
2025-08-28 21:59:59 +0530

    intersection

---
+++ namasteDSA/arrays/intersectionOf2Array.js
@@ -0,0 +1,11 @@
+var intersection = function(nums1, nums2) {
+    let set1 = new Set(nums1)
+    let set2 = new Set(nums2)
+let res = new Set()
+    for(let n of nums1){
+        if(set2.has(n)){
+res.add(n)
+        }
+    }
+    return [...res]
+};
