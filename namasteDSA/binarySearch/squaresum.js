090f34a444e2a067f27092aa0ae0bafba9b5ef15
ARUN-M-S <arunms96official@gmail.com>
2025-08-29 22:31:59 +0530

    sqr sum

--- namasteDSA/binarySearch/squaresum.js
+++ namasteDSA/binarySearch/squaresum.js
@@ -1,15 +1,15 @@
-var judgeSquareSum = function(c) {
-    if(c<=2) return true;
- let num1 = 0; let num2 = Math.floor(Math.sqrt(c));
- while(num1<=num2){
-    let sum = (num1*num1) + (num2*num2);
-    if(sum==c) return true;
-    if(sum<c){
-        num1++
-    }else{
-        num2--
+var judgeSquareSum = function (c) {
+    if (c <= 2) return true;
+    let num1 = 0; let num2 = Math.floor(Math.sqrt(c));
+    while (num1 <= num2) {
+        let sum = (num1 * num1) + (num2 * num2);
+        if (sum == c) return true;
+        if (sum < c) {
+            num1++
+        } else {
+            num2--
+        }
     }
- }
- return false;
-    
+    return false;
+
 };
