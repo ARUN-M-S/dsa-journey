7b1644824bbaf63c332455d2e5f89c746932e8c2
ARUN-M-S <arunms96official@gmail.com>
2025-08-31 23:23:14 +0530

    combination

---
+++ namasteDSA/backTrack/combination.js
@@ -0,0 +1,21 @@
+var combine = function (n, k) {
+    let result = [];
+    let backTrack = (path, i) => {
+        if (path.length == k) {
+            result.push([...path]);
+            return
+        }
+        for (let j = i; j <= n; j++) {
+
+            path.push(j)
+            backTrack(path, j + 1)
+            path.pop()
+        }
+
+
+
+
+    }
+    backTrack([], 1)
+    return result
+};
