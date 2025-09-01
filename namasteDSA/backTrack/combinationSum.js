7b37ab70c30016fb6702f82d8814f1fc7baa448c
ARUN-M-S <arunms96official@gmail.com>
2025-09-01 22:26:12 +0530

    combination sum

---
+++ namasteDSA/backTrack/combinationSum.js
@@ -0,0 +1,22 @@
+var combinationSum = function(candidates, target) {
+    let result =[];
+
+    let backTrack=(path,start,sum)=>{
+        if(sum==target){
+            result.push([...path])
+            return;
+        }
+        if (sum > target) return;
+        for(let i =start;i< candidates.length;i++){
+            path.push(candidates[i]);
+            backTrack(path,i,sum+candidates[i]);
+            path.pop()
+        }
+
+
+
+    }
+    backTrack([],0,0);
+    return result;
+    
+};
