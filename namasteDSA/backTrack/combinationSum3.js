91eaaee2e72dc70ef25c42c96b58b07837bdc4c4
ARUN-M-S <arunms96official@gmail.com>
2025-09-02 22:59:31 +0530

    combination sum 3

---
+++ namasteDSA/backTrack/combinationSum3.js
@@ -0,0 +1,20 @@
+var combinationSum3 = function(k, n) {
+    let result = [];
+    // candidates.sort((a, b) => a - b)
+    let backTrack = (path, start, sum) => {
+        if (sum == n && path.length==k) {
+            result.push([...path]);
+            return
+        };
+        if (sum > n || path.length>k) return;
+        for (let i = start; i <=9; i++) {
+           
+            path.push(i);
+            backTrack(path, i + 1, sum + i);
+            path.pop()
+        }
+    }
+    backTrack([], 1, 0)
+    return result;
+
+};
