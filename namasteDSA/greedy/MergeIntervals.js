be29db39b8d668345d1fcb4aeef6bdef1edb5a00
ARUN-M-S <arunms96official@gmail.com>
2025-09-20 23:14:12 +0530

    merge intervals

---
+++ namasteDSA/greedy/MergeIntervals.js
@@ -0,0 +1,13 @@
+var merge = function(intervals) {
+    intervals.sort((a,b)=>(a[0]-b[0]))
+    let ans = [intervals[0]];
+
+    for(let i =1;i<intervals.length;i++){
+        if(ans[ans.length-1][1]>=intervals[i][0]){
+            ans[ans.length-1][1] = Math.max(intervals[i][1],ans[ans.length-1][1])
+        }else{
+            ans.push(intervals[i])
+        }
+    }
+    return ans
+};
