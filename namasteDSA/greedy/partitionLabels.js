3678bc6210b493f8046b8c3aca6d1bc933646047
ARUN-M-S <arunms96official@gmail.com>
2025-09-21 21:50:18 +0530

    go thorugh each elemnt and find first and last postion

--- namasteDSA/greedy/partitionLabels.js
+++ namasteDSA/greedy/partitionLabels.js
@@ -2,4 +2,12 @@ var partitionLabels = function(s) {
     let ans = [];
     let first = Array(26).fill(-1)
     let last = Array(26).fill(-1);
+    for(let i =0;i<s.length;i++){
+        let char = s.charCodeAt(i)-97;
+        if(first[char]<0){
+            first[char]=i;
+        }else{
+            last[char]=i;
+        }
+    }
 }
