60aeb2ab96a9975b906f23a8aafe55be6ef4f46b
ARUN-M-S <arunms96official@gmail.com>
2025-09-30 23:28:27 +0530

    dp

--- namasteDSA/dynamicProgram/nThFibanocciNumber.js
+++ namasteDSA/dynamicProgram/nThFibanocciNumber.js
@@ -1,8 +1,12 @@
 function fib(n){
     if(n<=1)return n;
-    let map = {}
-    if(map[n])return map[n]
-    map[n]=fib(n-1)+fib(n-2)
-    return map[n]
+    let dp = new Array(n+1);
+    dp[0]=0;
+    dp[1]=1;
+    for(let i =2;i<=n;i++){
+        dp[i] = dp[i-1]+dp[i-2]
+
+    }
+    return dp[n]
 }
 console.log(fib(6))
