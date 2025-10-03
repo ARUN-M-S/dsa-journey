96b875f71edcd681ba6cbd76126a34bb58a4e2e2
ARUN-M-S <arunms96official@gmail.com>
2025-10-03 23:17:32 +0530

    decode updations

--- namasteDSA/dynamicProgram/decodeWays.js
+++ namasteDSA/dynamicProgram/decodeWays.js
@@ -1,21 +1,23 @@
 var numDecodings = function(s) {
-   
-    let fn= (remS)=>{
-        if(remS=='0') return 0;
-        if(remS=='')return 1;
-         let n = remS.length
-        let oneDigit=remS.substring(n-1);
-         let twoDigit=remS.substring(n-2);
-         let ans =0;
-         if(oneDigit!=0){
-            ans+=fn(remS.substring(0,n-1))
-         }
-         if(twoDigit>=10 && twoDigit<=26){
-ans+=fn(remS.substring(0,n-2))
-         }
-         return ans
-
-    }
-    return fn(s)
-    
-};
+    let dp ={}
+     let fn= (remS)=>{
+         if(remS=='0') return 0;
+         if(remS=='')return 1;
+         if(dp[remS]) return dp[remS]
+          let n = remS.length
+         let oneDigit=remS.substring(n-1);
+          let twoDigit=remS.substring(n-2);
+          let ans =0;
+          if(oneDigit!=0){
+             ans+=fn(remS.substring(0,n-1))
+          }
+          if(twoDigit>=10 && twoDigit<=26){
+ ans+=fn(remS.substring(0,n-2))
+          }
+          dp[remS]=ans
+          return ans
+ 
+     }
+     return fn(s)
+     
+ };
