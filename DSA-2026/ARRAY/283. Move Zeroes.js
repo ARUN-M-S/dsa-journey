97726de98b3c85d7798e4ecb78d4099abc454826
ARUN-M-S <arunms96official@gmail.com>
2026-01-27 22:39:01 +0530

    283. Move Zeroes

---
+++ DSA-2026/ARRAY/283. Move Zeroes.js
@@ -0,0 +1,16 @@
+
+ var moveZeroes = function(nums) {
+    let count=0; 
+    for(let i =0;i<nums.length;i++){
+        if(nums[i]!=0){
+            let temp = nums[i]
+            nums[i]=0;
+            nums[i-count]=temp
+
+        }else{
+            count++
+        }
+    }
+    return nums
+    
+};
