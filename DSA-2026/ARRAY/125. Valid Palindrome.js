0e3380d6c04b40464eedabda28a29ae3bb953c8b
ARUN-M-S <arunms96official@gmail.com>
2026-01-27 22:15:00 +0530

    125. Valid Palindrome

---
+++ DSA-2026/ARRAY/125. Valid Palindrome.js
@@ -0,0 +1,20 @@
+
+ var isPalindrome = function(s) {
+    let left =0;
+    let right = s.length-1;
+    while(left<right){
+        console.log(s[left])
+        if(!s[left].match(/[a-z0-9]/i)){
+            left++
+        }else if(!s[right].match(/[a-z0-9]/i)){
+            right --;
+        }else if (s[right].toLowerCase()==s[left].toLowerCase()){
+            left++;
+            right--
+        }else{
+            return false
+        }
+    }
+    return true;
+    
+};
