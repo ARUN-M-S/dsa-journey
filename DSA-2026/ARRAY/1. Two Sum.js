17362712cc7f0a8f1f2f3ff1bed2b9a03bdd0f1b
ARUN-M-S <arunms96official@gmail.com>
2026-01-27 08:58:20 +0530

    two sum using map

--- DSA-2026/ARRAY/1. Two Sum.js
+++ DSA-2026/ARRAY/1. Two Sum.js
@@ -10,3 +10,19 @@ var twoSum = function(nums, target) {
     return []
     
 };
+
+
+
+var twoSum = function(nums, target) {
+    let map  = new Map();
+    for(let i =0;i<nums.length;i++){
+        let compliment = target - nums[i]
+        if(map.has(compliment)){
+            return [i,map.get(compliment)]
+        }else{
+           map.set(nums[i],i)
+        }
+    }
+    return []
+    
+};
