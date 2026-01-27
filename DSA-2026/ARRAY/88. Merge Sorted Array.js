fbc6c27eecdb152bd85704f7b1f3c41be249a86d
ARUN-M-S <arunms96official@gmail.com>
2026-01-27 21:59:38 +0530

    88. Merge Sorted Array

---
+++ DSA-2026/ARRAY/88. Merge Sorted Array.js
@@ -0,0 +1,21 @@
+var merge = function (nums1, m, nums2, n) {
+
+    let k = nums1.length - 1;
+    while (m > 0 && n > 0) {
+        if (nums1[m - 1] > nums2[n]) {
+            nums1[k] = nums1[m - 1];
+            m--
+        } else {
+            nums1[k] = nums2[n - 1];
+            n--
+        }
+        k--
+    }
+    while (n > 0) {
+        nums1[k] = nums2[n - 1];
+        n--
+        k--
+    }
+    return nums1;
+
+};
