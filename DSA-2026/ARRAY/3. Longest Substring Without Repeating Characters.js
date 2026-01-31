6ead4cf60a8ec94b60550b9b5e2afb8abbd8f824
ARUN-M-S <arunms96official@gmail.com>
2026-01-31 13:32:57 +0530

    Longest sub string

---
+++ DSA-2026/ARRAY/3. Longest Substring Without Repeating Characters.js
@@ -0,0 +1,16 @@
+var lengthOfLongestSubstring = function(s) {
+    let max=0;
+    let set = new Set();
+    let L=0;
+    for(let i =0;i<s.length;i++){
+    while(set.has(s[i])){
+            set.delete(s[L])
+            L++;
+
+        }
+        max= Math.max(max,i-L+1)
+        set.add(s[i])
+    }
+    return max;
+    
+};
