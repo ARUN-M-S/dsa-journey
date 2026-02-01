fce5eea96875c8ce3f2d74e94dbbfbc57f5ed7c1
ARUN-M-S <arunms96official@gmail.com>
2026-02-01 16:14:49 +0530

    first occurance

---
+++ DSA-2026/ARRAY/Two Pointers/28. Find the Index of the First Occurrence in a String.js
@@ -0,0 +1,17 @@
+var strStr = function(haystack, needle) {
+    let L =0;
+
+    for(let i =0;i<haystack.length;i++){
+        if(haystack[i]==needle[L]){
+        if(L==needle.length-1) return i-L
+            L++
+        
+        }else{
+           
+            i=i-L
+             L=0
+        }
+
+    }
+    return -1
+};
