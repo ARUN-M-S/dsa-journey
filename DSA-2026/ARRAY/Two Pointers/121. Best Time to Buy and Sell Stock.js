313e891ba4649817f43e0cda1e922cce38aeb6c4
ARUN-M-S <arunms96official@gmail.com>
2026-02-02 21:34:57 +0530

    best time to sell

--- DSA-2026/ARRAY/Two Pointers/121. Best Time to Buy and Sell Stock.js
+++ DSA-2026/ARRAY/Two Pointers/121. Best Time to Buy and Sell Stock.js
@@ -1,15 +1,15 @@
-var maxProfit = function(prices) {
+var maxProfit = function (prices) {
     let maxProfit = 0;
-    let L =0;
-  
- for(let R=1;R<prices.length;R++){
-    if(prices[L]<prices[R]){
-        maxProfit= Math.max(maxProfit,prices[R]-prices[L])
-    }else{
-L=R
+    let L = 0;
+
+    for (let R = 1; R < prices.length; R++) {
+        if (prices[L] < prices[R]) {
+            maxProfit = Math.max(maxProfit, prices[R] - prices[L])
+        } else {
+            L = R
+        }
+
     }
-    
-  }
-  return maxProfit
-    
+    return maxProfit
+
 };
