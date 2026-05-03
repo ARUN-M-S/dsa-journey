8bcbbd0ccf46f8ca7cfdd861d0554fed13e73765
ARUN-M-S <arunms96official@gmail.com>
2026-05-03 16:08:05 +0530

    long sub str

--- arrays/arrayProblems/longestSubstring.js
+++ arrays/arrayProblems/longestSubstring.js
@@ -1,23 +1,18 @@
 function longestSub(str) {
     if (str.length==0) return str.length;
     let slow = 0;
-    let fast = 0;
-    let map = new Map();
+    let set = new Set()
     let maxSubString = 0;
-    while (fast < str.length) {
-        let curr = str[fast]
-        map.set(curr, (map.get(curr) || 0) + 1);
-        while (map.get(curr) > 1) {
-            map.set(str[slow], (map.get(str[slow]) || 0) - 1);
-
+    for(let fast =0;fast<str.length;fast++){
+        while(set.has(str[fast])){
+            set.delete(str[slow]);
             slow++
         }
-        maxSubString = Math.max(maxSubString, fast - slow)
-        fast++
-
-
+        set.add(str[fast])
+        maxSubString= Math.max(maxSubString, fast-slow+1)
     }
-    return maxSubString + 1
+    
+    return maxSubString
 
 }
 console.log(longestSub("abcabcbb"))
