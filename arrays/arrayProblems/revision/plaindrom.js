8cbd6a6cc0b5a758aeb3fe443d3665eab81a54ca
ARUN-M-S <arunms96official@gmail.com>
2026-05-04 20:19:23 +0530

    validatePalindrom revision

---
+++ arrays/arrayProblems/revision/plaindrom.js
@@ -0,0 +1,20 @@
+function validatePalindrom(str){
+    if(str.length<=1)return true;
+    let L=0;
+    let R= str.length-1;
+    while(L<R){
+        let leftChar = str[L].toLowerCase();
+        let rightChar = str[R].toLowerCase()
+        while(L<R && !isAlphaNumeric(str[L]))L++;
+        while(L<R && !isAlphaNumeric(str[R]))R--;
+        if(leftChar!=rightChar)return false;
+        L++;
+        R--
+    }
+    return true;
+}
+
+function isAlphaNumeric(char){
+    return  /[a-z0-9]/i.test(char)
+}
+console.log(validatepalindrom('A man, a plan, a canal: Panamaa'))
