1f873f78a8e727e9ee537216661acaefb87d9194
ARUN-M-S <arunms96official@gmail.com>
2026-05-05 23:07:09 +0530

    removeElemnt

---
+++ arrays/arrayProblems/removeElement.js
@@ -0,0 +1,13 @@
+var removeElement = function(nums, val) {
+    let fast= 0;
+    let slow =0;
+    while(fast<nums.length){
+        if(nums[fast]!=val){
+            nums[slow]=nums[fast];
+            slow++
+        }
+        fast++
+    }
+    return slow
+    
+};
