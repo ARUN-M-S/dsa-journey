217601f187189c691e5a0ec5d2910a01034cc52e
ARUN-M-S <arunms96official@gmail.com>
2026-05-05 15:15:57 +0530

    yaml file

--- arrays/user.js
+++ arrays/user.js
@@ -27,3 +27,19 @@ function fetchData(){
 
 
 fetchData().then(result=>console.log(result)).catch(error=>console.log(error))
+
+
+let x= 78538;
+
+function findCount(num){
+    if(num==0)return 1;
+    num = Math.abs(num)
+    let count=0;
+    while(num>0){
+        count++;
+        num= Math.floor(num/10)
+    }
+    return count;
+
+}
+console.log(findCount(x));
