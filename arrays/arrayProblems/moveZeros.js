37a5bac94883f115745ee86810334f89b234acca
ARUN-M-S <arunms96official@gmail.com>
2026-05-06 23:41:56 +0530

    move zeros snowfall way

--- arrays/arrayProblems/moveZeros.js
+++ arrays/arrayProblems/moveZeros.js
@@ -1,12 +1,25 @@
+// var moveZeroes = function(nums) {
+//     let slow = 0;
+//     let fast = 0;
+//     while(fast<nums.length){
+//         if(nums[fast]!=0){
+//             [nums[fast],nums[slow]]=[nums[slow],nums[fast]]
+//             slow++;
+//         }
+//         fast++
+//     }
+    
+// };
+
 var moveZeroes = function(nums) {
-    let slow = 0;
-    let fast = 0;
-    while(fast<nums.length){
-        if(nums[fast]!=0){
-            [nums[fast],nums[slow]]=[nums[slow],nums[fast]]
-            slow++;
+
+    let count = 0
+    for(let i =0;i<nums.length;i++){
+        if(nums[i]==0)count++;
+        else if (count>0){
+            nums[i-count] = nums[i]
+            nums[i]=0
         }
-        fast++
     }
     
 };
