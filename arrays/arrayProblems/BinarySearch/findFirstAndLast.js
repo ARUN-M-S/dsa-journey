5f6715f61845fe64712dc1ad560ad7367769c55c
ARUN-M-S <arunms96official@gmail.com>
2026-05-08 21:02:04 +0530

    findFirstAndLast

--- arrays/arrayProblems/BinarySearch/findFirstAndLast.js
+++ arrays/arrayProblems/BinarySearch/findFirstAndLast.js
@@ -1,27 +1,28 @@
 function findFirstAndLast(arr,t){
     let L =0;
     let R = arr.length-1;
-    let result=[]
+    let result=[-1,-1];
+
+    while(L<=R){
+        let M = L +Math.floor((R-L)/2);
+        if(arr[M]==t){
+            result[0]=M;
+            R=M-1
+        }else if(arr[M]>t) R=M-1;
+        else L=M+1
+    }
+     L =0;
+     R = arr.length-1;
+    while(L<=R){
+        let M = L +Math.floor((R-L)/2);
+        if(arr[M]==t){
+            result[1]=M;
+            L=M+1
+        }else if(arr[M]>t) R=M-1;
+        else L=M+1
+    }
     
-        while(L<R){
-            let mid = L +Math.floor((R-L)/2);
-            if(arr[mid]==t){
-                if(result.length>0){
-                    if( result[0]<mid)result[0]=mid;
-                    else result[1]=mid;
-                    R=mid-1;
-                    L=mid+1
-                }else{
-                    result[0]=mid;
-                }
-               
-            }else if (arr[mid]>t) R=mid-1;
-            else L=mid+1
-    
-        }
-        return result
-        
-   
+   return result
 
    
 }
