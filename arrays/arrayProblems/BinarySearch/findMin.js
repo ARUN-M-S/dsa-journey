9416bf41a8decaf5984b64ac2910d94795aa4202
ARUN-M-S <arunms96official@gmail.com>
2026-05-12 22:33:08 +0530

    find minimum sorted array

---
+++ arrays/arrayProblems/BinarySearch/findMin.js
@@ -0,0 +1,22 @@
+var findMin = function (arr) {
+    let L = 0;
+    let R = arr.length - 1;
+    while (L < R) {
+        let M = L + Math.floor((R - L) / 2);
+        if (arr[L] < arr[M]) {
+            if (arr[M] > arr[R]) {
+                L = M+1
+            } else {
+                R = M
+            }
+        } else {
+            if (arr[M] < arr[R]) {
+                R = M
+            } else {
+                L = M+1
+            }
+        }
+
+    }
+    return arr[L]
+};
