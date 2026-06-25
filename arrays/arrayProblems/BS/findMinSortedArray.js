f85d81b5622e11b14da09c7ec7a9174808f13b5b
ARUN-M-S <arunms96official@gmail.com>
2026-06-25 07:38:28 +0530

    findMin

--- arrays/arrayProblems/BS/findMinSortedArray.js
+++ arrays/arrayProblems/BS/findMinSortedArray.js
@@ -1,3 +1,16 @@
 function findMin(nums){
-    
+    let l =0;
+    let r = nums.length-1;
+    while(l<r){
+        let m = Math.floor(l+(r-l)/2);
+        if(nums[m]<nums[r]){
+            r=m;
+        }else{
+            l=m+1;
+        }
+    }
+    return nums[l];
 }
+
+console.log(findMin([4,5,6,7,0,1,2]))
+console.log(findMin([0,1,2,4,5,6,7]))
