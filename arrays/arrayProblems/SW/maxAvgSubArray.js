fa3d6765bef5a6092381b1bbbba09d7d3dfdcd9b
ARUN-M-S <arunms96official@gmail.com>
2026-06-30 00:14:20 +0530

    643. Maximum Average Subarray I

--- arrays/arrayProblems/SW/maxAvgSubArray.js
+++ arrays/arrayProblems/SW/maxAvgSubArray.js
@@ -1,20 +1,18 @@
+var findMaxAverage = function (nums, k) {
+  if (nums.length <= 1) return nums[0];
+  let sum = 0;
+  let avg = -Infinity;
+  let l = 0;
+  let r = 0;
+  while (r < nums.length) {
+    sum += nums[r];
+    if (r - l + 1 == k) {
+      avg = Math.max(avg, sum / k);
 
- var findMaxAverage = function(nums, k) {
-    if(nums.length<=1) return nums[0]
-    let sum =0;
-    let avg =-Infinity;
-    let l = 0;
-    let r=0;
-    while(r<nums.length){
-       sum+=nums[r];
-       if((r-l)+1==k){
-       avg= Math.max(avg,sum/k);
-       
-       sum-=nums[l];
-       l++;
-       }
-       r++
+      sum -= nums[l];
+      l++;
     }
-    return avg
-    
+    r++;
+  }
+  return avg;
 };
