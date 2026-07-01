f0ec8cc7f2be5bc6f1c7b8ecff31edebedd166c6
ARUN-M-S <arunms96official@gmail.com>
2026-07-01 23:40:10 +0530

    maxNovowels

--- arrays/arrayProblems/SW/maxNoVowels.js
+++ arrays/arrayProblems/SW/maxNoVowels.js
@@ -1,5 +1,23 @@
-function maxVowels(nums){
+var maxVowels = function (s, k) {
+    if (k > s.length) return -1;
+    let i = 0;
+    let j = 0;
+    let maxCount = 0;
     let vowels = new Set('aeiou')
-    console.log(vowels)
-}
-maxVowels('k')
+    let count = 0;
+
+    while (j < s.length) {
+        if(vowels.has(s[j] )&& j-i+1<=k){
+            count++
+        }else if(j-i+1>k){
+            if(vowels.has(s[i]))count--;
+            if(vowels.has(s[j]))count++;
+            i++;
+        }
+        j++;
+        maxCount= Math.max(maxCount,count)
+
+    }
+
+    return maxCount
+};
