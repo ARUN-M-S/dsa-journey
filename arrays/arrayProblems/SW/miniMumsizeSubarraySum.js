85c934ca06bd56e53692906e1d94aa881f0b8c80
ARUN-M-S <arunms96official@gmail.com>
2026-07-03 20:26:44 +0530

    minSubArrayLen

---
+++ arrays/arrayProblems/SW/miniMumsizeSubarraySum.js
@@ -0,0 +1,19 @@
+var minSubArrayLen = function (target, nums) {
+
+    let min = Infinity;
+
+    let l = 0;
+
+    let sum = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        sum += nums[i];
+        while (sum >= target) {
+            min = Math.min(min, i - l + 1)
+            sum -= nums[l]
+            l++
+        }
+    }
+
+    return min == Infinity ? 0 : min
+};
