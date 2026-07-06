70ddf23312eea2dff6de130b3fc972c3d1604e2e
ARUN-M-S <arunms96official@gmail.com>
2026-07-06 23:40:42 +0530

    revision of patterns

--- arrays/arrayProblems/revision/June6/minimumDaysRequiredForShip.js
+++ arrays/arrayProblems/revision/June6/minimumDaysRequiredForShip.js
@@ -1,32 +1,32 @@
-function minDays(w,d){
+function minDays(w, d) {
     let l = Math.max(...w);
-    let r = w.reduce((a,b)=>a+b,0);
-    
-    while(l<r){
-        let m = l+Math.floor((r-l)/2)
-        
-        if(helper(w,m,d)){
-            r=m
-        }else{
-            l=m+1
+    let r = w.reduce((a, b) => a + b, 0);
+
+    while (l < r) {
+        let m = l + Math.floor((r - l) / 2)
+
+        if (helper(w, m, d)) {
+            r = m
+        } else {
+            l = m + 1
         }
     }
     return l;
 }
 
-function helper(w,m,d){
-    let sum =0;
-    let count =1;
-    for(let i=0;i< w.length;i++){
-        sum+=w[i];
-        if(sum>m){
-            sum=w[i];
+function helper(w, m, d) {
+    let sum = 0;
+    let count = 1;
+    for (let i = 0; i < w.length; i++) {
+        sum += w[i];
+        if (sum > m) {
+            sum = w[i];
             count++
         }
     }
-    
-    
-    return count<=d
+
+
+    return count <= d
 }
 
-console.log(minDays([1,2,3,4,5,6,7,8,9,10],5))
+console.log(minDays([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 5))
