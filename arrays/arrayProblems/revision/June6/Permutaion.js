7071dda657d82b0ec64c4cd1ff827c8e795862a2
ARUN-M-S <arunms96official@gmail.com>
2026-07-09 23:53:27 +0530

    567. Permutation in String

--- arrays/arrayProblems/revision/June6/Permutaion.js
+++ arrays/arrayProblems/revision/June6/Permutaion.js
@@ -40,3 +40,41 @@ var checkInclusion = function (s1, s2) {
     return false
 
 };
+
+
+var checkInclusion = function (s1, s2) {
+    let s1Map = {}
+    for (let i = 0; i < s1.length; i++) {
+        s1Map[s1[i]] = (s1Map[s1[i]] || 0) + 1
+    }
+    let i = 0;
+    let j = 0;
+    let k = s1.length;
+    let s2Map = {}
+
+
+    while (j < s2.length) {
+
+        s2Map[s2[j]] = (s2Map[s2[j]] || 0) + 1;
+        if (j - i + 1 == k) {
+            let count = true
+            for (let ch in s1Map) {
+                if (s1Map[ch] != s2Map[ch]) {
+                    count=false;
+                    break;
+                }
+
+            }
+            if (count) return true;
+            s2Map[s2[i]] = (s2Map[s2[i]] || 0) - 1;
+            i++
+
+
+        }
+        j++
+    }
+    
+
+    return false
+
+};
