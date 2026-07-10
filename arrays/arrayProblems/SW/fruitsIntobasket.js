26a050fe913a9264c33c56b53fed0b217b65ee84
ARUN-M-S <arunms96official@gmail.com>
2026-07-10 23:26:58 +0530

    totalFruit

--- arrays/arrayProblems/SW/fruitsIntobasket.js
+++ arrays/arrayProblems/SW/fruitsIntobasket.js
@@ -1,5 +1,15 @@
 function totalFruit(fruits){
     if(fruits.length<1) return -1;
-    
+    let l =0;
+    let r =0;
+    let maxFruit =0;
+    let map = new Map()
+
+    for(let r=0;r<fruits.length;r++){
+        let fruit = fruits[r]
+        map.set()
+
+    }
+
 
 }
