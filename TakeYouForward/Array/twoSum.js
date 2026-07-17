88dea3a21555e121f5148849d39d9fc826e0f47f
ARUN-M-S <arunms96official@gmail.com>
2026-07-18 00:02:45 +0530

    1 of 75

---
+++ TakeYouForward/Array/twoSum.js
@@ -0,0 +1,15 @@
+function twoSum(nums,target){
+    let map ={};
+
+    for(let i =0;i<nums.length;i++){
+        let complement = target - nums[i];
+        if(complement in map){
+            return [i,map[target-nums[i]]]
+        }
+        map[nums[i]] = i
+
+    }
+    return []
+}
+
+console.log(twoSum([1, 6, 2, 10, 3],7))
