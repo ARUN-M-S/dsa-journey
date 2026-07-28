369b2a949760175d47d28313fb13551af169c132
ARUN-M-S <arunms96official@gmail.com>
2026-07-28 22:14:05 +0530

    revision

--- DSA-2026/SORT/revision/mergeSort.js
+++ DSA-2026/SORT/revision/mergeSort.js
@@ -18,7 +18,5 @@ function mergeSort(arr){
     let n = arr.length;
     let left = mergeSort(arr.slice(0,Math.floor(n/2)))
     let right = mergeSort(arr.slice(Math.floor(n/2)))
-
-    
     return merge(left,right)
 }
