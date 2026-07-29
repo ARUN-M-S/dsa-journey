6cf94a0889bb48e32d072477d2d51906e07f1ea9
ARUN-M-S <arunms96official@gmail.com>
2026-07-29 23:49:53 +0530

    insertion sort

--- DSA-2026/SORT/insertionSort.js
+++ DSA-2026/SORT/insertionSort.js
@@ -1,3 +1,16 @@
 function insertionSort(arr){
+
+    for(let i =1;i<arr.length;i++){
+        let curr = arr[i];
+        let prev = i-1
+
+        while(arr[prev]>curr && prev>=0){
+            arr[prev+1]=arr[prev];
+            prev--
+        }
+        arr[prev+1] = curr
+    }
     return arr
 }
+
+console.log(insertionSort([4,3,6,7,8,2,1]))
