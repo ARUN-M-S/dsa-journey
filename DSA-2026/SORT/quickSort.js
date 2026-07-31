74eb0fac89a34c306b79fe612d30ddab31afd571
ARUN-M-S <arunms96official@gmail.com>
2026-07-31 23:11:28 +0530

    quick sort helper

--- DSA-2026/SORT/quickSort.js
+++ DSA-2026/SORT/quickSort.js
@@ -1,28 +1,26 @@
-function quickSort(arr,startIndex,endIndex){
-    if(startIndex<endIndex){
-        let pivot = findPivot(arr,startIndex,endIndex)
-        quickSort(arr,startIndex,pivot-1)
-        quickSort(arr,pivot+1,endIndex)
-    }
+function quickSort(arr, startIndex, endIndex) {
+  if (startIndex < endIndex) {
+    let pivot = findPivot(arr, startIndex, endIndex);
+    quickSort(arr, startIndex, pivot - 1);
+    quickSort(arr, pivot + 1, endIndex);
+  }
 
-return arr
-
+  return arr;
 }
 
-function findPivot(arr,start,end){
-    let pivot = arr[end];
+function findPivot(arr, start, end) {
+  let pivot = arr[end];
 
-    let pos = start;
+  let pos = start;
 
-    for(let i =start;i<end;i++){
-        if(arr[i]<pivot ){
-            [arr[i],arr[pos]]= [arr[pos],arr[i]]
-            pos++
-        }
+  for (let i = start; i < end; i++) {
+    if (arr[i] < pivot) {
+      [arr[i], arr[pos]] = [arr[pos], arr[i]];
+      pos++;
     }
-    [arr[end],arr[pos]]= [arr[pos],arr[end]]
-    return pos
+  }
+  [arr[end], arr[pos]] = [arr[pos], arr[end]];
+  return pos;
 }
 
-
-console.log(quickSort([4,3,5,7,8,9,2,1,0,23],0,9))
+console.log(quickSort([4, 3, 5, 7, 8, 9, 2, 1, 0, 23], 0, 9));
