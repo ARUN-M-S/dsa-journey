ac13c1c17375819befd34df47df8e40dade3623f
ARUN-M-S <arunms96official@gmail.com>
2026-07-31 23:08:08 +0530

    quick sort helper

--- DSA-2026/SORT/quickSort.js
+++ DSA-2026/SORT/quickSort.js
@@ -1,3 +1,22 @@
 function quickSort(arr,startIndex,endIndex){
-    
+
 }
+
+function findPivot(arr,start,end){
+    let pivot = arr[end];
+
+    let pos = start;
+
+    for(let i =start;i<end;i++){
+        if(arr[i]<pivot ){
+            [arr[i],arr[pos]]= [arr[pos],arr[i]]
+            pos++
+        }
+    }
+ 
+    [arr[end],arr[pos]]= [arr[pos],arr[end]]
+    return pos
+}
+
+
+console.log(findPivot([4,3,5,7,8,9,2,1,0,23],0,9))
