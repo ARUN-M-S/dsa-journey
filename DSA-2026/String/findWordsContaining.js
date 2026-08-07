e8f985cf87659efecd4b30c362b48244a22f5a44
ARUN-M-S <arunms96official@gmail.com>
2026-08-07 22:07:29 +0530

    findWordsContaining

---
+++ DSA-2026/String/findWordsContaining.js
@@ -0,0 +1,12 @@
+function findWordsContaining(str,char){
+    let arr = [];
+    for(let i =0;i<str.length;i++){
+        for(let j =0;j<str[i].length;j++){
+            if(str[i][j]==char){
+                arr.push(i,j);
+                break
+            }
+        }
+    }
+    return arr
+}
