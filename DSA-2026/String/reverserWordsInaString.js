aecd88620bc7ace75c08c9e68b211e5a08d3b9f7
ARUN-M-S <arunms96official@gmail.com>
2026-08-09 22:04:50 +0530

    reverse word

--- DSA-2026/String/reverserWordsInaString.js
+++ DSA-2026/String/reverserWordsInaString.js
@@ -1,8 +1,25 @@
+// var reverseWords = function(s) {
+//     s = s.split(' ');
+//    return s.filter(Boolean).reverse().join(' ')
+//  };
+
+
 var reverseWords = function(s) {
     s = s.split(' ');
-   return s.filter(Boolean).reverse().join(' ')
+  let left = 0;
+  let right= s.length-1;
+  
+  while(left<right){
+      while(s[left]=='')left++;
+      while(s[right]=='')right--;
+      let temp = s[left];
+      s[left]=s[right];
+      s[right]=temp;
+      left++;
+      right--
+  }
+  return s.filter(Boolean).join(' ')
  };
-
  let s =  "the sky is blue"
  
 
@@ -10,3 +27,5 @@ var reverseWords = function(s) {
  s= "  hello world  "
 
  console.log(reverseWords(s));
+
+ console.log(reverseWords("a good   example"));
