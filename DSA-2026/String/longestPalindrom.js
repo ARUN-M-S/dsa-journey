a77b95c0971af3cf7a89246ec42cf73c7b023a8c
ARUN-M-S <arunms96official@gmail.com>
2026-08-14 22:30:02 +0530

    longest palindrom

--- DSA-2026/String/longestPalindrom.js
+++ DSA-2026/String/longestPalindrom.js
@@ -1,23 +1,22 @@
 
-function longestPalindrome(str){
-    
+function longestPalindrome(str) {
     let n = str.length;
-    if(n<=1)return str;
-   let result = '';
-   function expand(left,right){
-       while(left>=0&&right<n&&str[left]==str[right]){
-           left--;
-           right++
-       }
-       return str.slice(left+1,right)
-   }
-   for(let i =0;i<n;i++){
-       let odd = expand(i,i)
-       let even = expand(i,i+1);
-       if (odd.length > result.length) result = odd;
+    if (n <= 1) return str;
+    let result = '';
+    function expand(left, right) {
+        while (left >= 0 && right < n && str[left] == str[right]) {
+            left--;
+            right++
+        }
+        return str.slice(left + 1, right)
+    }
+    for (let i = 0; i < n; i++) {
+        let odd = expand(i, i)
+        let even = expand(i, i + 1);
+        if (odd.length > result.length) result = odd;
         if (even.length > result.length) result = even;
-   }
-   return result
+    }
+    return result
 }
 
 
