47341137148694e2fe180da3821d69068cb08e7f
ARUN-M-S <arunms96official@gmail.com>
2026-08-16 22:27:50 +0530

    sort colours

---
+++ DSA-2026/SORT/SORTColors.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+ var sortColors = function(nums) {
+
+    let left =0;
+    let right = nums.length-1;
+    let mid = 0;
+    while(mid<=right){
+      
+        if(nums[mid]==0){
+            swap(nums,left,mid);
+            left++
+            mid++
+        }else if(nums[mid]==2){
+            swap(nums,right,mid);
+            right--
+        }else{
+            swap(nums,left,mid);
+            mid++ 
+        }
+    }
+    return nums
+};
+
+function swap(arr,l,i){
+    console.log(arr)
+    return [arr[l],arr[i]]=[arr[i],arr[l]]
+}
+
+console.log(sortColors([2,0,2,1,1,0]))
