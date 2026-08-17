eee5e48f75286bc4eabf1a313dfaa36ab09b33cb
ARUN-M-S <arunms96official@gmail.com>
2026-08-17 23:51:20 +0530

    numSubarrayProductLessThanK

--- DSA-2026/ARRAY/numSubarrayProductLessThanK.js
+++ DSA-2026/ARRAY/numSubarrayProductLessThanK.js
@@ -1,22 +1,23 @@
 function numSubarrayProductLessThanK(arr,k){
+    if (k <= 1) return 0;
     let left=0;
     let right = 0;
     let n = arr.length;
     let result=[]
 let prdt = 1;
+let count=0;
 
-    while(right<=n){
-    prdt*=arr[right];
-    if(prdt<=k){
-        result.push([arr[right]])
-    }
-    while(prdt>k){
+for(let i =0;i<n;i++){
+    prdt*=arr[i];
+    while(prdt>=k && left<=i){
         prdt/=arr[left];
         left++
     }
-    right++
-    }
-    return result
+    count += right - left + 1;
+}
+
+   return count
+ 
 }
 
 console.log(numSubarrayProductLessThanK([10, 5, 2, 6], 100));
