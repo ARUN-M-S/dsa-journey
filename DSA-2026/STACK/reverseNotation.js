eee3d883548eb101d4c99be3ceb0a0acb31f11ce
ARUN-M-S <arunms96official@gmail.com>
2026-09-02 19:00:38 +0530

    reverseNOtation

---
+++ DSA-2026/STACK/reverseNotation.js
@@ -0,0 +1,26 @@
+var evalRPN = function(arr) {
+    let stack = []
+    let obj ={
+        '+':(a,b)=>a+b,
+        '-':(a,b)=>a-b,
+        '*':(a,b)=>a*b,
+        '/':(a,b)=>Math.trunc(a/b)
+    }
+
+    for(let char of arr){
+        if(isNaN(char)){
+            let b = stack.pop();
+            let a = stack.pop()
+            stack.push(obj[char](a,b))
+        }else{
+            stack.push(Number(char))
+        }
+        
+    }
+    return stack.pop()
+
+
+}
+
+console.log(evalRPN(['4', '13', '5', '/', '+']))
+
