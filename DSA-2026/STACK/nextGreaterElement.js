b2015131fa59bf928371f7911bf08a5ac7bfbcc5
ARUN-M-S <arunms96official@gmail.com>
2026-09-06 21:09:12 +0530

    next greater element

--- DSA-2026/STACK/nextGreaterElement.js
+++ DSA-2026/STACK/nextGreaterElement.js
@@ -9,22 +9,21 @@ var nextGreaterElement = function (nums1, nums2) {
 
     for (let i = n - 2; i >= 0; i--) {
         let j = stack.length
-      
 
-            while (stack.length > 0 && stack[stack.length - 1] < nums2[i]) {
-                console.log(stack)
-                stack.pop();
-            }
 
-            if (stack.length > 0) obj[nums2[i]] = stack[stack.length - 1]
-            else {
-                obj[nums2[i]] = -1
-            }
-        stack.push(nums2[i])
-
+        while (stack.length > 0 && stack[stack.length - 1] < nums2[i]) {
+            console.log(stack)
+            stack.pop();
         }
 
-    
+        if (stack.length > 0) obj[nums2[i]] = stack[stack.length - 1]
+        else {
+            obj[nums2[i]] = -1
+        }
+        stack.push(nums2[i])
+
+    }
+
     let result = []
     for (let i = 0; i < nums1.length; i++) {
         result.push(obj[nums1[i]])
