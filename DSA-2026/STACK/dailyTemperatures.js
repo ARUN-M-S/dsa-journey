918fc9997521d19a7c46e2de7d6990fc6d5f2ec5
ARUN-M-S <arunms96official@gmail.com>
2026-09-09 22:04:06 +0530

    alternative approch

--- DSA-2026/STACK/dailyTemperatures.js
+++ DSA-2026/STACK/dailyTemperatures.js
@@ -15,3 +15,30 @@ var dailyTemperatures = function(t) {
     return ans
    
 };
+
+
+var dailyTemperatures = function(t) {
+    let stack =[];
+    let n = t.length
+    let ans = Array(n).fill(0);
+    
+    stack.push(n-1)
+   
+    for(let i =n-1;i>=0;i--){
+      
+       while(stack.length){
+           let top = stack[stack.length-1];
+           if(t[i]>=t[top])stack.pop();
+           else {
+               ans[i]=top-i;
+               break;
+           }
+
+          
+       }
+   
+       stack.push(i)
+    }
+    return ans
+   
+};
