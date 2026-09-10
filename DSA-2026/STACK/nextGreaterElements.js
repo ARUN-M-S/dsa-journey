e997f0426a7247fa18849cfb747a147a446a73e1
ARUN-M-S <arunms96official@gmail.com>
2026-09-10 21:42:40 +0530

    second approch

--- DSA-2026/STACK/nextGreaterElements.js
+++ DSA-2026/STACK/nextGreaterElements.js
@@ -17,3 +17,27 @@ var nextGreaterElements = function(nums) {
     }
     return ans.slice(0,n/2)
 };
+
+
+var nextGreaterElements = function(nums) {
+    let arr = [...nums];
+    let n = arr.length;
+    let stack =[];
+    let ans =Array(n).fill(-1)
+    for(let i=n*2-1;i>=0;i--){
+        let j;
+        if(i>=n) j=i-n;
+        else j=i
+        while(stack.length){
+            let top = stack.length-1
+            if(stack[top]>arr[j]){
+                ans[j]=stack[top]
+                break;
+            }else{
+                stack.pop()
+            }
+        }
+        stack.push(arr[j])
+    }
+    return ans
+};
