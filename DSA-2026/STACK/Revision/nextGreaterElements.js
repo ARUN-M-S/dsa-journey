3b849d135e0ead888ff49f85be509606f3f0d76c
ARUN-M-S <arunms96official@gmail.com>
2026-09-11 22:08:42 +0530

    revision

---
+++ DSA-2026/STACK/Revision/nextGreaterElements.js
@@ -0,0 +1,43 @@
+var nextGreaterElements = function(nums) {
+    let arr = [...nums,...nums];
+    let n = arr.length;
+    let stack =[];
+    let ans =Array(n).fill(-1)
+    for(let i=n-1;i>=0;i--){
+        while(stack.length){
+            let top = stack.length-1
+            if(stack[top]>arr[i]){
+                ans[i]=stack[top]
+                break;
+            }else{
+                stack.pop()
+            }
+        }
+        stack.push(arr[i])
+    }
+    return ans.slice(0,n/2)
+};
+
+
+var nextGreaterElements = function(nums) {
+    let arr = [...nums];
+    let n = arr.length;
+    let stack =[];
+    let ans =Array(n).fill(-1)
+    for(let i=n*2-1;i>=0;i--){
+        let j;
+        if(i>=n) j=i-n;
+        else j=i
+        while(stack.length){
+            let top = stack.length-1
+            if(stack[top]>arr[j]){
+                ans[j]=stack[top]
+                break;
+            }else{
+                stack.pop()
+            }
+        }
+        stack.push(arr[j])
+    }
+    return ans
+};
