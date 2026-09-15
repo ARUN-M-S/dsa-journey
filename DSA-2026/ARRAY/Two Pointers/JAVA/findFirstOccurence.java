42761fe53730c1c5751c333f06bfa8f2040d41f7
ARUN-M-S <arunms96official@gmail.com>
2026-09-15 22:02:44 +0530

    revision

--- DSA-2026/ARRAY/Two Pointers/JAVA/findFirstOccurence.java
+++ DSA-2026/ARRAY/Two Pointers/JAVA/findFirstOccurence.java
@@ -1,3 +1,19 @@
+
+public class findFirstOccurence {
+    public static void main(String[] args) {
+        // 1. Create an instance of the Solution class
+        Solution solution = new Solution();
+
+        // 2. Define your input strings
+        String haystack = "sadbutsad";
+        String needle = "sad";
+        // 3. Call the method and store the result
+        int index = solution.strStr(haystack, needle);
+
+        // 4. Print the output (Expected: 0)
+        System.out.println("The needle starts at index: " + index);
+    }
+}
 class  Solution {
     public int strStr(String hay,String needle){
          int j =0;
@@ -6,6 +22,7 @@ class  Solution {
              if(j==needle.length()-1){
                  return i-j;
              }
+             j++;
             }else{
                 i=i-j;
                 j=0;
